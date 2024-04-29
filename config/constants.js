dc8466b9fde9a84e4cb3a3319bfadc0582ab07d3
TanjibQuad <tanjib@quadque.tech>
2024-04-29 10:00:19 +0600

    latest

---
+++ config/constants.js
@@ -0,0 +1,20 @@
+const keys = require('./keys')
+
+const auth = {
+  type: "OAuth2",
+  user: "megatanjib@gmail.com",
+  clientId: keys.google.clientID,
+  clientSecret: keys.google.clientSecret,
+  refreshToken: keys.google.refresh_token,
+};
+
+const mailoptions = {
+  from: "Quemailer <megatanjib@gmail.com>",
+  to: "tanjib@quadque.tech",
+  subject: "Gmail API NodeJS",
+};
+
+module.exports = {
+  auth,
+  mailoptions,
+};
--- routes/gmail-router.js
+++ routes/gmail-router.js
@@ -4,17 +4,22 @@ const {
   validateNotificationStoreData,
 } = require("../src/middleware/notificationStoreValidator");
 const {
-  // login,
-  // logout,
-  // callback,
-  list,
-} = require("../src/controllers/GmailAuthController");
+  getUser,
+  sendMail,
+  getDrafts,
+  readMail,
+} = require("../src/controllers/GmailApiController");
 
 //////////////////// routers  start ////////////////
 // gmailRouter.route("/google/login").get(login);
 // gmailRouter.route("/google/callback").get(callback);
 // gmailRouter.route("/google-logout").get(logout);
-gmailRouter.route("/list").get(list);
+// gmailRouter.route("/list").get(list);
+
+gmailRouter.route("/mail/user/:email").get(getUser);
+gmailRouter.route("/mail/send").get(sendMail);
+gmailRouter.route("/mail/drafts/:email").get(getDrafts);
+gmailRouter.route("/mail/read/:messageId").get(readMail);
 
 //////////////////// routers  end ///////////////
 
