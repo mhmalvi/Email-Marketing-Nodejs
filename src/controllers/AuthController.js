3687159a0163f76dd3b876c254d1b1f10acbf27b
TanjibQuad <tanjib@quadque.tech>
2024-04-29 12:31:19 +0600

    token delete on logout

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Token = require("../../models").Token;
 
 const logout = async (req, res) => {
+  console.log(req.body.token);
   const token = await Token.findOne({ where: { token: req.body.token } });
   if (token) {
     const result = await token.destroy();
