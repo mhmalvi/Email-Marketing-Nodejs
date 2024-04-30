2c341860efe1086a466e20e448a0dc8cfb3fa7b3
TanjibQuad <tanjib@quadque.tech>
2024-04-30 16:45:36 +0600

    insert contact

---
+++ routes/contact-routes.js
@@ -0,0 +1,7 @@
+const Contact = require("../models").Contact;
+const contactRouter = express.Router();
+const { saveContact } = require("../src/controllers/ContactInsertController");
+
+contactRouter.route("/contact-save").post(saveContact);
+
+module.exports = contactRouter;
