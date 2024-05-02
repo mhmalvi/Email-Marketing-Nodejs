4c8dd891522634585127351f2e4ecc13cbe96943
TanjibQuad <tanjib@quadque.tech>
2024-05-02 12:30:26 +0600

    insert contact

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -101,12 +101,12 @@ authRouter.get("/success", isLoggedIn, async (req, res) => {
   //   status: 200,
   //   user: userData,
   // });
-  res.send(
-    "Welcome" +
-      JSON.stringify(req.user.displayName) +
-      ".Your email is " +
-      req.user.email
-  );
+  // res.send(
+  //   "Welcome" +
+  //     JSON.stringify(req.user.displayName) +
+  //     ".Your email is " +
+  //     req.user.email
+  // );
   request(
     {
       url: `https://quemailer.com/home?username=${req.user.displayName}&email=${req.user.email}&googleID=${req.user.id}`,
