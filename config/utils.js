47d4a990d608ce11793462713b3f20c0a1b7c959
TanjibQuad <tanjib@quadque.tech>
2024-05-02 16:39:47 +0600

    get user

--- config/utils.js
+++ config/utils.js
@@ -26,20 +26,20 @@ function generateOTP() {
   return Math.floor(1000 + randomNum);
 }
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false, // Use `true` for port 465, `false` for all other ports
-//   auth: {
-//     user: keys.mail.user,
-//     pass: keys.mail.pass,
-//   },
-// });
-let transporter = nodemailer.createTransport({
-  sendmail: true,
-  newline: "unix",
-  path: "/usr/sbin/sendmail",
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: keys.mail.user,
+    pass: keys.mail.pass,
+  },
 });
+// let transporter = nodemailer.createTransport({
+//   sendmail: true,
+//   newline: "unix",
+//   path: "/usr/sbin/sendmail",
+// });
 
 const getIp = () => {
   var ip = "";
