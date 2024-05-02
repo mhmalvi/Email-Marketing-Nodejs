7e173d85efbcd771f53321e4e636a40fb1468b74
TanjibQuad <tanjib@quadque.tech>
2024-05-02 10:07:28 +0600

    sendmail integration

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,41 +1,48 @@
 const express = require("express");
 const Token = require("../../models").Token;
 const User = require("../../models").User;
-const sendmail = require("sendmail");
+const sendmail = require("sendmail")();
 const { transporter, generateOTP } = require("../../config/utils");
 const { saveToken } = require("../common/utils");
-const keys = require('../../config/keys')
+const keys = require("../../config/keys");
 
 const isUserEmailExists = async (req, res) => {
   // const user = await User.findOne({ where: { email: req.body.email } });
   // if (user) {
-    const otp = generateOTP();
-    // user.otp = otp;
-    // console.log(user);
-    // await user.save();
+  const otp = generateOTP();
+  // user.otp = otp;
+  // console.log(user);
+  // await user.save();
 
-    // await transporter.sendMail({
-    //   to: req.body.email, // list of receivers
-    //   subject: "Password verification", // Subject line
-    //   text: `Your Password is ${otp}`, // plain text body
-    //   // html: "<b>Hello world?</b>", // html body
-    // });
-    sendmail(
-      {
-        from: keys.mail.user,
-        to: req.body.email,
-        subject: "Password verification",
-        html: `Your Password is ${otp}`,
-      },
-      function (err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-      }
-    );
-    res.status(200).json({
-      status: true,
-      password: otp,
-    });
+  await transporter.sendMail(
+    {
+      from: keys.mail.user,
+      to: req.body.email, // list of receivers
+      subject: "Password verification", // Subject line
+      text: `Your Password is ${otp}`, // plain text body
+      // html: "<b>Hello world?</b>", // html body
+    },
+    (err, info) => {
+      console.log(info.envelope);
+      console.log(info.messageId);
+    }
+  );
+  // await sendmail(
+  //   {
+  //     from: keys.mail.user,
+  //     to: req.body.email,
+  //     subject: "Password verification",
+  //     html: `Your Password is ${otp}`,
+  //   },
+  //   function (err, reply) {
+  //     console.log(err && err.stack);
+  //     console.dir(reply);
+  //   }
+  // );
+  res.status(200).json({
+    status: true,
+    password: otp,
+  });
   // } else {
   //   res.status(404).json({
   //     status: false,
