a55be29a94f84beff30922b4ed3d14c6f4f7397d
TanjibQuad <tanjib@quadque.tech>
2024-05-08 14:16:19 +0600

    latest

--- src/controllers/ContactInsertController.js
+++ src/controllers/ContactInsertController.js
@@ -1,7 +1,24 @@
 const express = require("express");
-
+const Contact = require("../../models").Contact;
+const { saveContact } = require("../common/contactsUtils.js/saveContact");
 const saveContact = async (req, res) => {
-    console.log(req.body);
-}
+  const json = req.body;
+  if (response) {
+    await json.forEach((element) => {
+      saveContact(element);
+    });
+    res.status(201).json({
+      message: "Contact inserted",
+      status: 201,
+      contact: json,
+    });
+  } else {
+    res.status(403),
+      json({
+        message: "No data given",
+        status: 403,
+      });
+  }
+};
 
-module.exports = {saveContact}
+module.exports = { saveContact };
