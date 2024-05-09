5886e1c2c7bf8e79023d5511a4dc4f509a8cfa3e
TanjibQuad <tanjib@quadque.tech>
2024-05-09 16:17:30 +0600

    sent user id to client side during google login

--- src/common/contactsUtils/fetch.js
+++ src/common/contactsUtils/fetch.js
@@ -1,7 +1,7 @@
 const Contact = require("../../../models").Contact;
 
-const fetch =  (data) => {
-  return Contact.findAll({
+const fetch =  async (data) => {
+  return await Contact.findAll({
     where: { user_id: data },
     order: [["id", "DESC"]],
   });
