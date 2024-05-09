7c9714dd523e7ecdcc8f3aa952bd3d3d262437a6
TanjibQuad <tanjib@quadque.tech>
2024-05-09 15:55:05 +0600

    sent user id to client side during google login

--- src/controllers/Contacts/ContactInsertController.js
+++ src/controllers/Contacts/ContactInsertController.js
@@ -11,8 +11,8 @@ const insertContact = async (req, res) => {
       const UserCollectionExist = JSON.stringify(fetch(data));
       const collection = JSON.parse(UserCollectionExist);
       console.log(collection);
-      if (collection !== null) {
-        collection.forEach((user) => {
+      if (collection) {
+        Object.keys(collection).forEach((user) => {
           console.log(user.json.email);
           if (element.email !== user.json.email) {
             saveContact(element, userID);
