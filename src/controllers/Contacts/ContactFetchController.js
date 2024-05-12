b86e414ebda5db1e938b47608dfbb1d623a3ceff
TanjibQuad <tanjib@quadque.tech>
2024-05-12 09:19:19 +0600

    pagination added

--- src/controllers/Contacts/ContactFetchController.js
+++ src/controllers/Contacts/ContactFetchController.js
@@ -6,9 +6,11 @@ const { getPagingData } = require("../../../config/utils");
 const fetchContact = async (req, res) => {
   const data = JSON.parse(req.body.userID);
   const page = req.body.page;
-  const limit = req.body.limit;
+  const size = req.body.size;
   if (data) {
+    const { limit, offset } = getPagination(page, size);
     const result = await fetch(data);
+    console.log(result);
     const response = await getPagingData(result, page, limit);
     if (response) {
       res.status(200).json({
