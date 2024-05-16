6a62416e53725cdf67bdb8aa6d9daa158fceb0b5
TanjibQuad <tanjib@quadque.tech>
2024-05-16 10:23:08 +0600

    pagination

--- src/controllers/Contacts/ContactFetchController.js
+++ src/controllers/Contacts/ContactFetchController.js
@@ -11,7 +11,7 @@ const fetchContact = async (req, res) => {
     // const { limit, offset } = getPagination(page, size);
     // console.log(limit);
     offset = (page-1)*page
-    const result = await fetch(data, limit, offset);
+    const result = await fetch(data, size, offset);
     // const response = await getPagingData(result, page, limit);
     console.log(result);
     if (result) {
