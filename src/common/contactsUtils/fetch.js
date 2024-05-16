728302beb043d4c89a4a03d6934d8534ecd1382c
TanjibQuad <tanjib@quadque.tech>
2024-05-16 10:00:35 +0600

    latest

--- src/common/contactsUtils/fetch.js
+++ src/common/contactsUtils/fetch.js
@@ -3,6 +3,7 @@ const Contact = require("../../../models").Contact;
 const fetch =  async (data) => {
   return await Contact.findAll({
     where: { user_id: data },
+    order: [["id", "DESC"]],limit:10
   });
 };
 
