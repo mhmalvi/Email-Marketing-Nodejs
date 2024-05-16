6407898c7ba459459cf568b5e314fff256f5d577
TanjibQuad <tanjib@quadque.tech>
2024-05-16 16:35:25 +0600

    update contact

--- src/common/contactsUtils/update.js
+++ src/common/contactsUtils/update.js
@@ -2,7 +2,7 @@ const Contact = require("../../../models").Contact;
 
 const updateOne = async (data) => {
   console.log(data);
-  await Contact.update({ json: data.json }, { where: { id: data.id } });
+  return await Contact.update({ json: data.json }, { where: { id: data.id } });
 };
 
 module.exports = { updateOne };
