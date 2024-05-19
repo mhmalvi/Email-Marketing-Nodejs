2c0091be3c280c0c81adc709a730622c8128b866
TanjibQuad <tanjib@quadque.tech>
2024-05-19 16:06:59 +0600

    template insert

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -1,15 +1,21 @@
-const express = require('express')
+const express = require("express");
+const Token = require("../../models").Token;
 
 module.exports.isUserAuthenticated = async (req, res, next) => {
-    if (req.user) {
-        next()
-    } else {
-        res.status(401).send('You must login')
-    }
-}
+  if (req.user) {
+    next();
+  } else {
+    res.status(401).send("You must login");
+  }
+};
 
 module.exports.isCustomerAuthenticated = async (req, res, next) => {
-  if (req.user) {
+  const bearerHeader = req.headers["authorization"];
+  // console.log(bearerHeader);
+  const isTokenExist = await Token.findOne({
+    where: { token: bearerHeader },
+  });
+  if (isTokenExist) {
     next();
   } else {
     res.status(401).send("You must login");
