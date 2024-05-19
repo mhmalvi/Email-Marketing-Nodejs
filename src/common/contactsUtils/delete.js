fa92c1a23199f5a1a7893fae43d64fe35c8e1b27
TanjibQuad <tanjib@quadque.tech>
2024-05-19 08:49:31 +0600

    delete contact

--- src/common/contactsUtils/delete.js
+++ src/common/contactsUtils/delete.js
@@ -2,7 +2,8 @@ const Contact = require("../../../models").Contact;
 
 const destroy = async (data) => {
   return await Contact.destroy({
-    where: { id: id },
+    where: { id: data.id },
+    where: { user_id: data.user_id },
   });
 };
 
