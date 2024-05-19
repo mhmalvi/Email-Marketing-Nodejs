8dd44d26a03b89fa7db0ad55c0ec307183aff6ba
TanjibQuad <tanjib@quadque.tech>
2024-05-19 08:45:42 +0600

    delete contact

---
+++ src/controllers/Contacts/ContactDestroyController.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Contact = require("../../../models").Contact;
+const { destroy } = require("../../common/contactsUtils/delete");
+const { findOne } = require("../../common/contactsUtils/findOne");
+const { getPagingData, getPagination } = require("../../../config/utils");
+
+const destroyContact = async (req, res) => {
+  let contact = await findOne(req.body.id);
+  if (contact) {
+    const result = await destroy(req.body);
+    console.log(result);
+    if (result[0] === 1) {
+      res.status(201).json({
+        message: "Deleted",
+        status: 201,
+      });
+    } else {
+      res.status(500).json({
+        message: "Failed",
+        status: 500,
+      });
+    }
+  } else {
+    res.status(404).json({
+      message: "Not found",
+      status: 404,
+    });
+  }
+};
+
+module.exports = { destroyContact };
