beff3a553c4679f18e2179997d40ab14828f4b76
TanjibQuad <tanjib@quadque.tech>
2024-05-19 09:28:42 +0600

    delete contact

--- src/controllers/Contacts/ContactDestroyController.js
+++ src/controllers/Contacts/ContactDestroyController.js
@@ -5,26 +5,33 @@ const { findOne } = require("../../common/contactsUtils/findOne");
 const { getPagingData, getPagination } = require("../../../config/utils");
 
 const destroyContact = async (req, res) => {
-  let contact = await findOne(req.body.id);
-  if (contact) {
-    const result = await destroy(req.body);
-    console.log(result);
-    if (result === 1) {
-      res.status(201).json({
-        message: "Deleted",
-        status: 201,
-      });
+  if (req.body.id && req.body.user_id) {
+    let contact = await findOne(req.body.id);
+    if (contact) {
+      const result = await destroy(req.body);
+      console.log(result);
+      if (result === 1) {
+        res.status(201).json({
+          message: "Deleted",
+          status: 201,
+        });
+      } else {
+        res.status(500).json({
+          message: "Failed",
+          status: 500,
+        });
+      }
     } else {
-      res.status(500).json({
-        message: "Failed",
-        status: 500,
+      res.status(404).json({
+        message: "Not found",
+        status: 404,
       });
     }
   } else {
-    res.status(404).json({
-      message: "Not found",
-      status: 404,
-    });
+      res.status(422).json({
+        message: "Please provide all the fields",
+        status: 422,
+      });
   }
 };
 
