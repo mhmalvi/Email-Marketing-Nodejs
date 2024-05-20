f3ed51e5bf2ef13daf08d8a8b02de476c28154e9
TanjibQuad <tanjib@quadque.tech>
2024-05-20 08:50:20 +0600

    destroy template

--- src/common/templateUtils/delete.js
+++ src/common/templateUtils/delete.js
@@ -2,7 +2,7 @@ const Template = require("../../../models").Template;
 
 const destroy = async (data) => {
   return await Template.destroy({
-    where: { id: data.id, user_id: data.user_id },
+    where: { id: data.id, client_id: data.client_id },
   });
 };
 
