89266c15607b8c10577041f3ae218ae9e98c21ac
TanjibQuad <tanjib@quadque.tech>
2024-05-20 08:50:20 +0600

    destroy template

--- src/controllers/Templates/TemplateDestroyController.js
+++ src/controllers/Templates/TemplateDestroyController.js
@@ -1,10 +1,27 @@
-// const {destroy}
+const { destroy } = require("../../common/templateUtils/delete");
 
 const templateDestroy = async (req, res) => {
-    console.log(req.body);
-    if (req.body.id && req.body.client_id) {
-
+  console.log(req.body);
+  if (req.body.id && req.body.client_id) {
+    const result = await destroy(req.body);
+    console.log(result);
+    if (result === 1) {
+      res.status(201).json({
+        message: "Template deleted",
+        status: 201,
+      });
+    } else {
+      res.status(500).json({
+        message: "Failed",
+        status: 500,
+      });
     }
+  } else {
+    res.status(422).json({
+      message: "Please provide all fields",
+      status: 422,
+    });
+  }
 };
 
 module.exports = { templateDestroy };
