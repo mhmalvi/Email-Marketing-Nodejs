a80c782a15ef3bd935c43d04c181cdd21dbdb771
TanjibQuad <tanjib@quadque.tech>
2024-05-20 11:53:40 +0600

    fetch groups

--- routes/group-router.js
+++ routes/group-router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const groupRouter = express.Router();
+const { isCustomerAuthenticated } = require("../src/middleware/userMiddleware");
 const {
   fetchGroups,
 } = require("../src/controllers/Groups/GroupFetchController");
 
-groupRouter.route("/group-fetch").post(fetchGroups);
+groupRouter.route("/group-fetch").post(isCustomerAuthenticated, fetchGroups);
 
 module.exports = { groupRouter };
