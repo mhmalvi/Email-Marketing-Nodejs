d03b4ec68d062ed8881f9c20631f2571cb04f912
TanjibQuad <tanjib@quadque.tech>
2024-05-21 10:32:41 +0600

    fetch contact

--- src/common/contactsUtils/fetchByGroup.js
+++ src/common/contactsUtils/fetchByGroup.js
@@ -1,7 +1,7 @@
 const Contact = require("../../../models").Contact;
 
-const fetchByGroup = async (user_id, group, size, offset) => {
-  console.log(size, offset);
+const fetchByGroup = async (user_id, group) => {
+//   console.log(size, offset);
   var contactsArray = [];
   const contacts = await Contact.findAll({
     where: { user_id: user_id },
