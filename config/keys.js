3118b14b22d034b7f2abd9f602133b6719ffa635
TanjibQuad <tanjib@quadque.tech>
2024-05-21 13:13:11 +0600

    email auth

--- config/keys.js
+++ config/keys.js
@@ -7,7 +7,7 @@ module.exports = {
       "1//042xetNeQbxRjCgYIARAAGAQSNwF-L9IrE3guTrAL95QH2EqdYWoS-eBSN16GB-2InXiLBKmvrEYTvaYAk2NilHVlQPf2p1Z8h7I",
   },
   mail: {
-    user: "tanjib@quadque.tech",
+    user: "tanjibrubyat@gmail.com",
     pass: "#Tricoderma123",
   },
   redirectUi: "https://backend.quemailer.com/google/callback",
