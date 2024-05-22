c7f8887c8966536479c205e51ff49a4ab8207396
TanjibQuad <tanjib@quadque.tech>
2024-05-22 12:19:51 +0600

    logout token destroy

--- config/utils.js
+++ config/utils.js
@@ -40,12 +40,9 @@ const transporter = nodemailer.createTransport({
   host: "localhost",
   port: 587,
   secure: false, // Use `true` for port 465, `false` for all other ports
-  // auth: {
-  //   user: keys.mail.user,
-  //   pass: keys.mail.pass,
-  // },
-  tls: {
-    rejectUnauthorized: false,
+  auth: {
+    user: keys.mail.user,
+    pass: keys.mail.pass,
   },
 });
 // let transporter = nodemailer.createTransport({
