f3e45d1b7e4a63564a8e7ef2500a69242fc8d947
TanjibQuad <tanjib@quadque.tech>
2024-05-23 11:23:33 +0600

    dsn configuration

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,8 +6,16 @@ const { transporter, generateOTP } = require("../../config/utils");
 const { randomAlphaNumeric } = require("../../config/utils");
 const { saveToken } = require("../common/utils");
 const keys = require("../../config/keys");
+const EmailValidator = require("email-deep-validator");
 
 const isUserEmailExists = async (req, res) => {
+  const emailValidator = new EmailValidator();
+  const { wellFormed, validDomain, validMailbox } = await emailValidator.verify(
+    req.body.email
+  );
+  console.log(wellFormed);
+  console.log(validDomain);
+  console.log(validMailbox);
   // const user = await User.findOne({ where: { email: req.body.email } });
   // if (user) {
   // const otp = generateOTP();
