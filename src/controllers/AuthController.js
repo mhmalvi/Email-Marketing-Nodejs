6cdbfafedab66b13d929213a87445fb4e8bd6b82
TanjibQuad <tanjib@quadque.tech>
2024-05-23 15:42:32 +0600

    template type json

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -18,49 +18,46 @@ const isUserEmailExists = async (req, res) => {
   // console.log(validMailbox);
   const user = await User.findOne({ where: { email: req.body.email } });
   if (user) {
-  const otp = generateOTP();
-  user.otp = otp;
-  console.log(user);
-  await user.save();
-  const mailOptions = {
-    from: "<tanjib@quadque.tech>",
-    to: req.body.email, // list of receivers
-    subject: "OTP verification", // Subject line
-    text: `Your OTP is ${otp}`,
-    // text: `Your OTP is`,
-    headers: {
-      "Return-Path": "megatanjib@gmail.com",
-    },
-    // Specify the return path address
-  };
-  // const envelopeOptions = {
-  //   from: "tanjib@quadque.tech", // Envelope sender address
-  // };
-  // console.log("Mail Options:", mailOptions);
-  const info = await transporter.sendMail(mailOptions, function (err, info) {
-    if (err) {
-      console.log(err);
-      return "Error while sending email" + err;
-    } else {
-      console.log("Email sent", info);
-      return "Email sent";
-    }
-  });
-  // await sendmail(
-  //   {
-  //     from: keys.mail.user,
-  //     to: req.body.email,
-  //     subject: "Password verification",
-  //     html: `Your Password is ${otp}`,
-  //   },
-  //   function (err, reply) {
-  //     console.log(err && err.stack);
-  //     console.dir(reply);
-  //   }
-  // );
-  res.status(200).json({
-    status: true,
-  });
+    const otp = generateOTP();
+    user.otp = otp;
+    console.log(user);
+    await user.save();
+    const mailOptions = {
+      from: "<tanjib@quadque.tech>",
+      to: req.body.email, // list of receivers
+      subject: "OTP verification", // Subject line
+      text: `Your OTP is ${otp}`,
+      // text: `Your OTP is`,
+      // Specify the return path address
+    };
+    // const envelopeOptions = {
+    //   from: "tanjib@quadque.tech", // Envelope sender address
+    // };
+    // console.log("Mail Options:", mailOptions);
+    const info = await transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+        return "Error while sending email" + err;
+      } else {
+        console.log("Email sent", info);
+        return "Email sent";
+      }
+    });
+    // await sendmail(
+    //   {
+    //     from: keys.mail.user,
+    //     to: req.body.email,
+    //     subject: "Password verification",
+    //     html: `Your Password is ${otp}`,
+    //   },
+    //   function (err, reply) {
+    //     console.log(err && err.stack);
+    //     console.dir(reply);
+    //   }
+    // );
+    res.status(200).json({
+      status: true,
+    });
   } else {
     res.status(404).json({
       status: false,
