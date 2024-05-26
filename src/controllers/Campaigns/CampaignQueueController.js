775b5c406713f7944677159326e75fa424a54294
TanjibQuad <tanjib@quadque.tech>
2024-05-26 14:27:14 +0600

    insert campaign

--- src/controllers/Campaigns/CampaignQueueController.js
+++ src/controllers/Campaigns/CampaignQueueController.js
@@ -2,12 +2,12 @@ const express = require("express");
 const Campaign = require("../../../models").Campaignqueue;
 const Emailqueue = require("../../../models").Emailqueue;
 const { saveCampaign } = require("../../common/campaignUtils/saveCampaign");
-const Campaignqueue = require("../../../models").Campaignqueue;
+const Campaignqueue = require("../../../models").CampaignQueue;
 
 const campaignQueue = async (req, res) => {
   console.log(req.body);
   //   const campaign = await saveCampaign(req.body);
-  await Campaignqueue.create({
+  const campaign = await Campaignqueue.create({
     fromMail: req.body.campaignInfo.fromMail,
     fromName: req.body.campaignInfo.fromName,
     count: 5,
