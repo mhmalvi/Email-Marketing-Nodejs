bdb6a3d603c99dabc8dc20c327465cee9f5f8d8d
TanjibQuad <tanjib@quadque.tech>
2024-05-26 15:21:56 +0600

    insert app password

--- src/controllers/AppPasswords/InsertAppPasswordController.js
+++ src/controllers/AppPasswords/InsertAppPasswordController.js
@@ -4,20 +4,27 @@ const { save } = require("../../common/appPassUtils/save");
 const { fetchOne } = require("../../common/appPassUtils/fetchOne");
 
 const saveAppPassword = async (req, res) => {
-    if (req.body.email && req.body.email && req.body.email) {
-      await fetchOne(req.body)
-    const app = await save(req.body);
-    if (app) {
-      res.status(201).json({
-        message: "Saved",
-        status: 201,
-        data: app,
+  if (req.body.email && req.body.email && req.body.email) {
+    const isEmailExist = await fetchOne(req.body);
+    if (isEmailExist) {
+      res.status(409).json({
+        message: "Email already exists",
+        status: 409,
       });
     } else {
-      res.status(500).json({
-        message: "Failed",
-        status: 500,
-      });
+      const app = await save(req.body);
+      if (app) {
+        res.status(201).json({
+          message: "Saved",
+          status: 201,
+          data: app,
+        });
+      } else {
+        res.status(500).json({
+          message: "Failed",
+          status: 500,
+        });
+      }
     }
   } else {
     res.status(422).json({
