690297d4cd9adfeb2399eb9d869b50c7fff2ee70
TanjibQuad <tanjib@quadque.tech>
2024-05-26 15:24:07 +0600

    insert app password

--- src/common/appPassUtils/fetchOne.js
+++ src/common/appPassUtils/fetchOne.js
@@ -2,7 +2,7 @@ const AppPassword = require("../../../models").AppPassword;
 
 const fetchOne = async (data) => {
   return await AppPassword.findOne({
-    where: { email: data.email,user },
+    where: { email: data.email, user_id: data.user_id },
   });
 };
 module.exports = { fetchOne };
