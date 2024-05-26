64fc62a51222084012d7183842b86330d5144eb1
TanjibQuad <tanjib@quadque.tech>
2024-05-26 16:42:36 +0600

    update app password

--- src/common/appPassUtils/updateOne.js
+++ src/common/appPassUtils/updateOne.js
@@ -3,7 +3,7 @@ const AppPassword = require("../../../models").AppPassword;
 const updateOne = async (data) => {
   console.log(data);
   return await AppPassword.update(
-    { email: data.email, app_password: data.app_password },
+    { email: data.email, app_password: data.appPassword },
     { where: { id: data.id, user_id: data.userID } }
   );
 };
