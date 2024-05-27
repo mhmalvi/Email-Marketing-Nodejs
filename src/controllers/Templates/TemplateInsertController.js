687e79d9dff0edd001ab80a37e0bb4fad8396941
TanjibQuad <tanjib@quadque.tech>
2024-05-27 14:46:16 +0600

    template save

--- src/controllers/Templates/TemplateInsertController.js
+++ src/controllers/Templates/TemplateInsertController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { saveTemplate } = require("../../common/templateUtils/insert");
 const { findOne } = require("../../common/templateUtils/findOne");
 const insertTemplate = async (req, res) => {
-  if (req.body.name && req.body.template && req.body.userID) {
+  if (req.body.name && req.body.template && req.body.user_id) {
     const response = await findOne(req.body);
     console.log(response);
     if (response === null) {
