f4456e4a7acd7df3824a2b17d52f02a6ab000847
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-05-28 09:45:32 +0600

    check if a single provider exists

--- src/common/appPassUtils/fetchOne.js
+++ src/common/appPassUtils/fetchOne.js
@@ -2,7 +2,7 @@ const AppPassword = require("../../../models").AppPassword;
 
 const fetchOne = async (data) => {
   return await AppPassword.findOne({
-    where: { id: data.email, user_id: data.userID },
+    where: { provider: data.provider, user_id: data.userID },
   });
 };
 module.exports = { fetchOne };
