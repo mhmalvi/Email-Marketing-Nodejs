37c95f5dc65fa5c9cd1414b15492421f38b21271
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-05-28 13:13:08 +0600

    emails scattering

--- src/common/appPassUtils/fetchAll.js
+++ src/common/appPassUtils/fetchAll.js
@@ -1,8 +1,24 @@
 const AppPassword = require("../../../models").AppPassword;
 
 const fetchAll = async (userID) => {
-  return await AppPassword.findAll({
-    where: { user_id: userID },
-  });
+  let google = AppPassword.findOne({
+    where: { user_id: userID, provider:"Google" },
+  })?AppPassword.findOne({
+    where: { user_id: userID, provider:"Google" },
+  }):null
+  let yahoo = AppPassword.findOne({
+    where: { user_id: userID, provider:"Yahoo" },
+  })?AppPassword.findOne({
+    where: { user_id: userID, provider:"Yahoo" },
+  }):null
+  let outlook = AppPassword.findOne({
+    where: { user_id: userID, provider:"Outlook" },
+  })?AppPassword.findOne({
+    where: { user_id: userID, provider:"Outlook" },
+  }):null
+
+  return emails = {
+    google,yahoo,outlook
+  }
 };
 module.exports = { fetchAll };
