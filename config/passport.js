8dc8de84675701945412c763cf7646f44a4b2023
TanjibQuad <tanjib@quadque.tech>
2024-05-29 14:42:24 +0600

    app password delete

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const { google } = require("googleapis");
 const keys = require("./keys");
 
 const CLIENT_ID =
