308e1a225489b5cdd28ea07853ccf05b4d010e03
TanjibQuad <tanjib@quadque.tech>
2024-05-29 14:41:20 +0600

    app password delete

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -8,18 +8,7 @@ const { google } = require("googleapis");
 const { saveCredentials } = require("../src/controllers/GmailAuthController");
 const { saveToken } = require("../src/common/utils");
 
-const CLIENT_ID =
-  "491721270445-154edfaqh36aac6lt07h7ea2a6ic8r9o.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
-const REDIRECT_URI = "https://backend.quemailer.com/google/callback";
-const REFRESH_TOKEN =
-  "1//042xetNeQbxRjCgYIARAAGAQSNwF-L9IrE3guTrAL95QH2EqdYWoS-eBSN16GB-2InXiLBKmvrEYTvaYAk2NilHVlQPf2p1Z8h7I";
 
-  const oauth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI
-  );
 
 authRouter.get("/home", (req, res) => {
   res.send("Home Page");
