31c1fc3643dfbf9ee01bca289d247b272866b77f
TanjibQuad <tanjib@quadque.tech>
2024-06-03 11:58:00 +0600

    add group column to contacts table

--- src/common/contactsUtils/fetchByGroupPagination.js
+++ src/common/contactsUtils/fetchByGroupPagination.js
@@ -1,11 +1,13 @@
 const Contact = require("../../../models").Contact;
 
-const fetchByGroup = async (user_id, group) => {
+const fetchByGroup = async (user_id, group, size, offset) => {
   //   console.log(size, offset);
   var contactsArray = [];
   const contacts = await Contact.findAll({
     where: { user_id: user_id, group: group },
     order: [["id", "DESC"]],
+    limit: size,
+    offset: offset,
   });
 
   // contacts.forEach((contact) => {
