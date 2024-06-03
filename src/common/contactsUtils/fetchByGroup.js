1ac01e21a2f4a678eef11df0bad5dfd20a314a28
TanjibQuad <tanjib@quadque.tech>
2024-06-03 08:42:28 +0600

    queue emails

--- src/common/contactsUtils/fetchByGroup.js
+++ src/common/contactsUtils/fetchByGroup.js
@@ -1,20 +1,22 @@
 const Contact = require("../../../models").Contact;
 
 const fetchByGroup = async (user_id, group) => {
-//   console.log(size, offset);
+  //   console.log(size, offset);
   var contactsArray = [];
   const contacts = await Contact.findAll({
     where: { user_id: user_id },
     order: [["id", "DESC"]],
   });
-
+  const count = 0;
   contacts.forEach((contact) => {
     console.log(contact);
     if (contact.json.group && contact.json.group === group) {
       contactsArray.push(contact);
+      count = contactsArray.length;
     }
   });
   //   console.log(contactsArray);
+  contactsArray.push(count);
   return contactsArray;
 };
 
