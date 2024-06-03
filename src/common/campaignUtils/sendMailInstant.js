8a06cea886bdd55a5619d8390c280e8c8dbc159e
TanjibQuad <tanjib@quadque.tech>
2024-06-03 14:03:52 +0600

    cron job

---
+++ src/common/campaignUtils/sendMailInstant.js
@@ -0,0 +1,24 @@
+const Emailqueue = require("../../../models").EmailQueue;
+const { transporter } = require("../../config/utils");
+
+const sendMail = async (req, res) => {
+  const mailOptions = {
+    from: "<tanjib@quadque.tech>",
+    to: "megatanjib@gmail.com", // list of receivers
+    subject: "OTP verification", // Subject line
+    text: `Your OTP is 4567`,
+    // text: `Your OTP is`,
+    // Specify the return path address
+  };
+
+  const info = await transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+      return "Error while sending email" + err;
+    } else {
+      console.log("Email sent", info);
+      return "Email sent";
+    }
+  });
+};
+module.exports = { sendMail };
