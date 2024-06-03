b882258ae9dea43363f8e75ee98be2cfef447107
TanjibQuad <tanjib@quadque.tech>
2024-06-03 10:50:55 +0600

    queue emails

--- src/common/contactsUtils/fetchByGroup.js
+++ src/common/contactsUtils/fetchByGroup.js
@@ -7,7 +7,7 @@ const fetchByGroup = async (user_id, group) => {
   // var filter = { "Contact.json.group": group, "Contact.user_id": user_id };
   const contacts = await Contact.findAll({
     // include: includes,
-    where: { ["$contact.json.group$"]: group, "$contact.user_id$": user_id },
+    where: { ["$contact.json.group$"]: group, ["$contact.user_id$"]: user_id },
     include: [{ model: Contact, as: "contact" }],
     order: [["id", "DESC"]],
   });
