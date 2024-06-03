a9d0658ed5eb2c50a448705134e81e22107d6926
TanjibQuad <tanjib@quadque.tech>
2024-06-03 08:48:26 +0600

    queue emails

--- src/common/contactsUtils/fetchByGroup.js
+++ src/common/contactsUtils/fetchByGroup.js
@@ -16,7 +16,7 @@ const fetchByGroup = async (user_id, group) => {
     }
   });
   //   console.log(contactsArray);
-  contactsArray.push(total=count);
+  contactsArray.push({total:count});
   return contactsArray;
 };
 
