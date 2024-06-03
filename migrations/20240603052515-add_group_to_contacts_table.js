e87a2c3618929829e016e213174eb991512df276
TanjibQuad <tanjib@quadque.tech>
2024-06-03 11:30:23 +0600

    add group column to contacts table

--- migrations/20240603052515-add_group_to_contacts_table.js
+++ migrations/20240603052515-add_group_to_contacts_table.js
@@ -9,7 +9,7 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn("contacts", "group", {
+    await queryInterface.addColumn("Contacts", "group", {
       type: Sequelize.TEXT,
       allowNull: false,
     });
