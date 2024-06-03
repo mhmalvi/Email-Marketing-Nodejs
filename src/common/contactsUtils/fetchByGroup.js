3f2531f8219c5e64806745453edc6b6a9f95f1ef
TanjibQuad <tanjib@quadque.tech>
2024-06-03 11:17:42 +0600

    queue emails

--- src/common/contactsUtils/fetchByGroup.js
+++ src/common/contactsUtils/fetchByGroup.js
@@ -1,29 +1,21 @@
 const Contact = require("../../../models").Contact;
-const fetchByGroup = async (user_id, group) => {
-  //   console.log(size, offset);
-  var contactsArray = [];
-  // var includes = [{ model: Contact, as: "Contact.json" }];
-  // var filter = { "Contact.json.group": group, "Contact.user_id": user_id };
-  const contacts = await Contact.findAll({
-    // include: includes,
-    include: [{ model: Contact, as: "parent" }],
-    where: {
-      [json.group]: group,
-      user_id: user_id,
-    },
 
+const fetchByGroup = async (user_id, group) => {
+//   console.log(size, offset);
+  var contactsArray = [];
+  const contacts = await Contact.findAll({
+    where: { user_id: user_id },
     order: [["id", "DESC"]],
   });
-  console.log(contacts);
-  // contacts.forEach((contact) => {
-  //   console.log(contact);
-  //   if (contact.json.group && contact.json.group === group) {
-  //     contactsArray.push(contact);
-  //   }
-  // });
+
+  contacts.forEach((contact) => {
+    console.log(contact);
+    if (contact.json.group && contact.json.group === group) {
+      contactsArray.push(contact);
+    }
+  });
   //   console.log(contactsArray);
-  // contactsArray.push({ total: count });
-  // return contactsArray;
+  return contactsArray;
 };
 
 module.exports = { fetchByGroup };
