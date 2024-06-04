2a786c9beb0235b8a9747ca4c32cdbf5eff54507
TanjibQuad <tanjib@quadque.tech>
2024-06-04 14:35:14 +0600

    insert contact fix

--- src/common/contactsUtils/checkIfContactExist.js
+++ src/common/contactsUtils/checkIfContactExist.js
@@ -1,6 +1,6 @@
 const Contact = require("../../../models").Contact;
 
-const ifContactExist = async (user_id, email) => {
+const ifContactExist = async (user_id) => {
   return await Contact.findAll({
     where: { user_id: user_id },
     attributes: ["email"],
