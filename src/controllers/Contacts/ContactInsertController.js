777e8760edbf2aef8094de36b6c318fdde453cfb
TanjibQuad <tanjib@quadque.tech>
2024-06-04 13:18:23 +0600

    insert contact fix

--- src/controllers/Contacts/ContactInsertController.js
+++ src/controllers/Contacts/ContactInsertController.js
@@ -2,24 +2,27 @@ const express = require("express");
 const Contact = require("../../../models").Contact;
 const { saveContact } = require("../../common/contactsUtils/saveContact");
 const { fetch } = require("../../common/contactsUtils/fetch");
-const { ifContactExist } = require("../../common/contactsUtils/checkIfContactExist");
+const {
+  ifContactExist,
+} = require("../../common/contactsUtils/checkIfContactExist");
 const insertContact = async (req, res) => {
   const json = req.body;
   console.log(json);
   const data = JSON.parse(req.params.userID);
   console.log(data);
   if (json.length > 0) {
-    // const UserCollectionExist = JSON.stringify(await fetch(data));
+    const UserCollectionExist = JSON.stringify(await fetch(data));
 
     await json.forEach((element) => {
       // console.log("string", UserCollectionExist);
       // const collection = JSON.parse(UserCollectionExist);
       // console.log("abc", collection);
       var count = 0;
-      const userCollectionExist = ifContactExist(data, element.email);
-      console.log("data", data);
-      console.log("userCollectionExist", userCollectionExist);
-      if (!userCollectionExist) {
+      if (UserCollectionExist.length > 0) {
+        if (!UserCollectionExist.includes(element.email)) {
+          saveContact(element, data);
+        }
+      } else {
         saveContact(element, data);
       }
       // else {
