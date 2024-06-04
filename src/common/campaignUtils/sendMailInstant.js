af17aad5ba8967dba0102a5a2b4818361ebe6201
TanjibQuad <tanjib@quadque.tech>
2024-06-04 16:18:54 +0600

    insert contact fix

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -3,25 +3,29 @@ const { transporter } = require("../../../config/utils");
 const { fetchQueuedMails } = require("./queueMail");
 
 const sendMail = async (req, res) => {
-  const mails = await fetchQueuedMails()
+  const mails = await fetchQueuedMails();
   console.log(mails);
-  // const mailOptions = {
-  //   from: "<tanjib@quadque.tech>",
-  //   to: "megatanjib@gmail.com", // list of receivers
-  //   subject: "OTP verification", // Subject line
-  //   text: `Your OTP is 4567`,
-  //   // text: `Your OTP is`,
-  //   // Specify the return path address
-  // };
+  mails.forEach(async (mail) => {
+    const mailOptions = {
+      from: "<tanjib@quadque.tech>",
+      to: mail.recipientEmail, // list of receivers
+      subject: "OTP verification", // Subject line
+      text: `Your OTP is 4567`,
+      // text: `Your OTP is`,
+      // Specify the return path address
+    };
+    const info = await transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+        return "Error while sending email" + err;
+      } else {
+        console.log("Email sent", info);
+        return "Email sent";
+      }
+      // });
+    });
 
-  // const info = await transporter.sendMail(mailOptions, function (err, info) {
-  //   if (err) {
-  //     console.log(err);
-  //     return "Error while sending email" + err;
-  //   } else {
-  //     console.log("Email sent", info);
-  //     return "Email sent";
-  //   }
-  // });
+    //
+  });
 };
 module.exports = { sendMail };
