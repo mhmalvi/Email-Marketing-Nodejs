efc86335c2ec27f01141c5837e5912ef0c0f8883
TanjibQuad <tanjib@quadque.tech>
2024-06-04 12:45:02 +0600

    insert contact fix

--- src/common/contactsUtils/saveContact.js
+++ src/common/contactsUtils/saveContact.js
@@ -3,6 +3,7 @@ const Contact = require("../../../models").Contact;
 const saveContact = (data, userID) => {
   return Contact.create({
     json: data,
+    email: data.email ? data.email : "",
     group: data.group ? data.group : "",
     user_id: userID,
   });
