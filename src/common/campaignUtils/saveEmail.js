42db898f9c5725a78b20b5dcbc22f39dfee69934
TanjibQuad <tanjib@quadque.tech>
2024-06-05 10:11:41 +0600

    insert contact fix

---
+++ src/common/campaignUtils/saveEmail.js
@@ -0,0 +1,22 @@
+const Emailqueue = require("../../../models").EmailQueue;
+
+const saveEmail = async () => {
+  try {
+    results.forEach(async (element) => {
+      await Emailqueue.create({
+        subject: data.campaignInfo.subject,
+        fromName: data.campaignInfo.fromName,
+        fromEmail: data.campaignInfo.fromMail,
+        recipientName: element.json.name,
+        recipientEmail: element.json.email,
+        group: element.json.group,
+        templateName: data.template.name,
+        templateData: data.template.data,
+        campaignID: campaignID,
+        userID: data.userID,
+      });
+    });
+  } catch (error) {}
+};
+
+module.exports = { saveEmail };
