a9e88af1eca9a92f0820a9567a70bca26ae62839
TanjibQuad <tanjib@quadque.tech>
2024-06-05 12:18:13 +0600

    insert contact fix

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -7,34 +7,31 @@ const AppPassword = require("../../../models").AppPassword;
 const { fetchQueuedMails } = require("./queueMail");
 const { updateDeliveryStatus } = require("./updateQueueMail");
 const sendMail = async (req, res) => {
-  const mails = await fetchQueuedMails();
+  const mails = await fetchQueuedMails(); /////////////  get queued recipients from db ///////////
   console.log(mails);
   mails.forEach(async (mail) => {
     const sender = await AppPassword.findOne({
       where: { email: mail.fromEmail },
-    });
+    }); ////////////  get app password of the sender from db //////////////////
     let transporterResponse = await transporter(sender);
     const mailOptions = {
       to: mail.recipientEmail, // list of receivers
       subject: mail.subject, // Subject line
-      text: mail.templateData,
+      text: mail.templateData, // email body
       // text: `Your OTP is`,
       // Specify the return path address
     };
-    const info = await transporterResponse.sendMail(
-      mailOptions,
-      async (err, info) => {
-        if (err) {
-          console.log(err);
-          return "Error while sending email" + err;
-        } else {
-          await updateDeliveryStatus(info.accepted);
-          console.log("Email sent", info.accepted);
-          return "Email sent";
-        }
-        // });
+    await transporterResponse.sendMail(mailOptions, async (err, info) => {
+      if (err) {
+        console.log(err);
+        return "Error while sending email" + err;
+      } else {
+        await updateDeliveryStatus(info.accepted);
+        console.log("Email sent", info.accepted);
+        return "Email sent";
       }
-    );
+      // });
+    });
 
     //
   });
