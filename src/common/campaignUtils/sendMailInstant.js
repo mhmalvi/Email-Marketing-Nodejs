f00a48bc4c43dae1e016a6f25c7a42a1f6e62d08
TanjibQuad <tanjib@quadque.tech>
2024-06-05 11:25:52 +0600

    insert contact fix

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -1,7 +1,8 @@
 const Emailqueue = require("../../../models").EmailQueue;
-const { transporter } = require("../../../config/utils");
+const {
+  transporter,
+} = require("../../common/transporterUtils/customTransporter");
 const nodemailer = require("nodemailer");
-const { customTransporter } = require("../transporterUtils/customTransporter");
 const AppPassword = require("../../../models").AppPassword;
 const { fetchQueuedMails } = require("./queueMail");
 const sendMail = async (req, res) => {
@@ -11,15 +12,16 @@ const sendMail = async (req, res) => {
     const sender = await AppPassword.findOne({
       where: { email: mail.fromEmail },
     });
-    const customTransporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // Use `true` for port 465, `false` for all other ports
-      auth: {
-        user: sender.email,
-        pass: sender.app_password,
-      },
-    });
+    let transporter = await transporter(sender);
+    // const customTransporter = nodemailer.createTransport({
+    //   host: "smtp.gmail.com",
+    //   port: 587,
+    //   secure: false, // Use `true` for port 465, `false` for all other ports
+    //   auth: {
+    //     user: sender.email,
+    //     pass: sender.app_password,
+    //   },
+    // });
     const mailOptions = {
       to: mail.recipientEmail, // list of receivers
       subject: mail.subject, // Subject line
@@ -27,19 +29,16 @@ const sendMail = async (req, res) => {
       // text: `Your OTP is`,
       // Specify the return path address
     };
-    const info = await customTransporter.sendMail(
-      mailOptions,
-      function (err, info) {
-        if (err) {
-          console.log(err);
-          return "Error while sending email" + err;
-        } else {
-          console.log("Email sent", info.accepted);
-          return "Email sent";
-        }
-        // });
+    const info = await transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+        return "Error while sending email" + err;
+      } else {
+        console.log("Email sent", info.accepted);
+        return "Email sent";
       }
-    );
+      // });
+    });
 
     //
   });
--- src/common/transporterUtils/customTransporter.js
+++ src/common/transporterUtils/customTransporter.js
@@ -1,15 +1,14 @@
-
-
 const transporter = async (data) => {
   const customTransporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: keys.mail.user,
-      pass: keys.mail.pass,
+      user: sender.email,
+      pass: sender.app_password,
     },
   });
+  return customTransporter;
 };
 
 module.exports = { transporter };
