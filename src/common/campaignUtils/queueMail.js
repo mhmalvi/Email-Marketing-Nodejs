0b2725bde0f0d2b8d9a1b1b0f4cadcf415402bb0
TanjibQuad <tanjib@quadque.tech>
2024-06-05 10:11:41 +0600

    insert contact fix

--- src/common/campaignUtils/queueMail.js
+++ src/common/campaignUtils/queueMail.js
@@ -1,29 +1,19 @@
-const Emailqueue = require("../../../models").EmailQueue;
+const { saveEmail } = require("./saveEmail");
+
 const Contact = require("../../../models").Contact;
 
 const queueMail = async (data, campaignID) => {
   try {
     console.log(data.userID);
-    console.log('group',data.recipient.group);
+    console.log("group", data.recipient.group);
     if (data.recipient.group) {
       const results = await Contact.findAll({
         where: { user_id: data.userID, group: data.recipient.group },
       });
       console.log(JSON.stringify(results));
-      results.forEach(async (element) => {
-        await Emailqueue.create({
-          subject: data.campaignInfo.subject,
-          fromName: data.campaignInfo.fromName,
-          fromEmail: data.campaignInfo.fromMail,
-          recipientName: element.json.name,
-          recipientEmail: element.json.email,
-          group: element.json.group,
-          templateName: data.template.name,
-          templateData: data.template.data,
-          campaignID: campaignID,
-          userID: data.userID,
-        });
-      });
+      const response = await saveEmail(data, results);
+      console.log(response);
+      // return 1;
     } else {
       await data.recipient.list.forEach(async (element) => {
         await Emailqueue.create({
