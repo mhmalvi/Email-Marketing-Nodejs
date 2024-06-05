1bc57bc5066c7dab1ddc6996912762aca28f2c40
TanjibQuad <tanjib@quadque.tech>
2024-06-05 12:33:30 +0600

    insert contact fix

--- src/common/contactsUtils/update.js
+++ src/common/contactsUtils/update.js
@@ -3,7 +3,7 @@ const Contact = require("../../../models").Contact;
 const updateOne = async (data) => {
   console.log(data);
   return await Contact.update(
-    { json: data.json },
+    { json: data.json, group: data.json.group ? data.json.group : "" },
     { where: { id: data.id, user_id: data.user_id } }
   );
 };
