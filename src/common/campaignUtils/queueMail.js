cce11fd700238d5f99c2e830cd64379c21668975
TanjibQuad <tanjib@quadque.tech>
2024-06-06 11:29:34 +0600

    schedule added to emailqueue model

--- src/common/campaignUtils/queueMail.js
+++ src/common/campaignUtils/queueMail.js
@@ -13,7 +13,7 @@ const queueMail = async (data, campaignID) => {
         recipientName: element.json.name,
         recipientEmail: element.json.email,
         group: element.json.group,
-        schedule: element.schedule,
+        schedule: data.schedule,
         templateName: data.template.name,
         templateData: data.template.data,
         campaignID: campaignID,
