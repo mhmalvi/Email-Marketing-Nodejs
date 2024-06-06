bd645d0bd55b3f8b89c148015e6444030d430182
TanjibQuad <tanjib@quadque.tech>
2024-06-06 11:42:27 +0600

    schedule added to emailqueue model

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -22,53 +22,54 @@ const sendMail = async (req, res) => {
     console.log("curr time", new Date());
     if (mail.schedule <= new Date()) {
       console.log("true");
+      const sender = await AppPassword.findOne({
+        where: { email: mail.fromEmail },
+      }); ////////////  get app password of the sender from db //////////////////
+
+      const template = convert(mail.templateData); ////////// convert from html to plain text /////////
+      const id = mail.id;
+      const file = path.join(__dirname, "../../ejs/mail.ejs");
+      const data = await ejs.renderFile(file, {
+        template,
+        id,
+      });
+      let transporterResponse = await transporter(sender);
+      const mailOptions = {
+        to: mail.recipientEmail, // list of receivers
+        subject: mail.subject, // Subject line
+        // text: mail.templateData, // email body
+        html: data,
+        // text: `Your OTP is`,
+        // Specify the return path address
+      };
+      const emailValidator = new EmailValidator();
+      const { wellFormed, validDomain, validMailbox } =
+        await emailValidator.verify(mail.recipientEmail);
+
+      console.log(wellFormed);
+      console.log(validDomain);
+      console.log(validMailbox);
+      if (!validDomain || !wellFormed) {
+        await updateBounceStatus(mail.id);
+      } else {
+        await transporterResponse.sendMail(mailOptions, async (err, info) => {
+          if (err) {
+            console.log(err);
+            return "Error while sending email" + err;
+          } else {
+            console.log(info.accepted[0]);
+
+            console.log("Email sent", info.accepted);
+            return "Email sent";
+          }
+          // });
+        });
+        await updateDeliveryStatus(mail.id);
+      }
     } else {
       console.log("false");
     }
-    // const sender = await AppPassword.findOne({
-    //   where: { email: mail.fromEmail },
-    // }); ////////////  get app password of the sender from db //////////////////
-
-    // const template = convert(mail.templateData); ////////// convert from html to plain text /////////
-    // const id = mail.id;
-    // const file = path.join(__dirname, "../../ejs/mail.ejs");
-    // const data = await ejs.renderFile(file, {
-    //   template,
-    //   id,
-    // });
-    // let transporterResponse = await transporter(sender);
-    // const mailOptions = {
-    //   to: mail.recipientEmail, // list of receivers
-    //   subject: mail.subject, // Subject line
-    //   // text: mail.templateData, // email body
-    //   html: data,
-    //   // text: `Your OTP is`,
-    //   // Specify the return path address
-    // };
-    // const emailValidator = new EmailValidator();
-    // const { wellFormed, validDomain, validMailbox } =
-    //   await emailValidator.verify(mail.recipientEmail);
-
-    // console.log(wellFormed);
-    // console.log(validDomain);
-    // console.log(validMailbox);
-    // if (!validDomain || !wellFormed) {
-    //   await updateBounceStatus(mail.id);
-    // } else {
-    //   await transporterResponse.sendMail(mailOptions, async (err, info) => {
-    //     if (err) {
-    //       console.log(err);
-    //       return "Error while sending email" + err;
-    //     } else {
-    //       console.log(info.accepted[0]);
-
-    //       console.log("Email sent", info.accepted);
-    //       return "Email sent";
-    //     }
-    //     // });
-    //   });
-    //   await updateDeliveryStatus(mail.id);
-    // } ////////////////////////////////////////////////////////////////
+    //  ////////////////////////////////////////////////////////////////
     // }
     // );
     //
