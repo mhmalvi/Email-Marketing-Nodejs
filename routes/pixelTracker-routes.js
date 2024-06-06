8df0816983439faa6786da013790c2e795bb79dd
TanjibQuad <tanjib@quadque.tech>
2024-06-06 09:41:32 +0600

    mail template

--- routes/pixelTracker-routes.js
+++ routes/pixelTracker-routes.js
@@ -4,7 +4,7 @@ const pixelTracker = express.Router();
 pixelTracker.get(`/open/:id`, (req, res) => {
   const pixelId = req.params.id;
   console.log(pixelId);
-  res.sendFile("1x1.png", { root: __dirname });
+  res.sendFile("1x1.png", { root: __dirname+'../' });
 });
 
 module.exports = { pixelTracker };
