411961cc9bc55cc115a06311f8416f83d126124c
TanjibQuad <tanjib@quadque.tech>
2024-06-10 16:42:51 +0600

    delete otp after 5 minutes

--- src/controllers/CronController.js
+++ src/controllers/CronController.js
@@ -2,11 +2,8 @@ const cron = require("node-cron");
 const { sendMail } = require("../common/campaignUtils/sendMailInstant");
 const { deleteOTPCron } = require("../common/cron/CronMethods/deleteOTPCron");
 
-const cronJob = async () => {
-  cron.schedule("* * * * *", () => {
-    console.log("job");
-    sendMail();
-    deleteOTPCron();
-  });
-};
-module.exports = { cronJob };
+cron.schedule("* * * * *", () => {
+  console.log("job");
+  sendMail();
+  deleteOTPCron();
+});
