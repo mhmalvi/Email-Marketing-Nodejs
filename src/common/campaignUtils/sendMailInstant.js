fa378dd7af63e7ff39dcf33ac6752f7e3c95c630
TanjibQuad <tanjib@quadque.tech>
2024-06-13 17:30:35 +0600

    send mail fix

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -54,7 +54,7 @@ const sendMail = async (req, res) => {
         to: mail.recipientEmail, // list of receivers
         subject: mail.subject, // Subject line
         // text: data, // email body
-        html: styledText,
+        html: data,
         // text: styledText,
         // Specify the return path address
       };
