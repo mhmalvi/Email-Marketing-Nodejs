de8639e48ee47941227e0ba6c72496bfe91cc4be
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-06-15 00:56:20 +0600

    email tracking

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -55,7 +55,7 @@ const sendMail = async (req, res) => {
         to: mail.recipientEmail, // list of receivers
         subject: mail.subject, // Subject line
         // text: data, // email body
-        text: data,
+        html: data,
         // text: styledText,
         // Specify the return path address
       };
