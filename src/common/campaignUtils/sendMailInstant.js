763d08a3ccb86291e086a029b34f36559345479e
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-06-14 23:25:43 +0600

    email tracking

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -48,7 +48,7 @@ const sendMail = async (req, res) => {
         pixel_url,
       });
       const $ = cheerio.load(data); ////////// load html to cheerio /////////
-      const styledText = $.html(); ////////// render html to plain text  /////////
+      const styledText = $('body').html(); ////////// render html to plain text  /////////
       let transporterResponse = await transporter(sender);
       const mailOptions = {
         to: mail.recipientEmail, // list of receivers
