c2da114950365f70f3c7a478e9c8b6dc6b1cbcf1
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-06-16 15:14:13 +0600

    email tracking

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -53,7 +53,7 @@ const sendMail = async (req, res) => {
       // });
       // Step 2: Read the Mustache template from a file.
       const templatePath = path.join(__dirname, "../../mustache/mail.mustache");
-      var template = fs.readFileSync(templatePath);
+      var template = fs.readFileSync(templatePath,'utf8');
       const data = {
         template: template,
         id: id,
