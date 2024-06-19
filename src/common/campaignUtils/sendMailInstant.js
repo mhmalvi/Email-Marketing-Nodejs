bf7b0e07de5d2403ca5ae3ec3c9d02084cb43f1f
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-06-19 11:16:01 +0600

    error fix

--- src/common/campaignUtils/sendMailInstant.js
+++ src/common/campaignUtils/sendMailInstant.js
@@ -91,6 +91,7 @@ const sendMail = async (req, res) => {
 
           // });
           await EmailQueue.update({ open: 0 }, { where: { id: id } });
+          id = null
         });
       }
       
