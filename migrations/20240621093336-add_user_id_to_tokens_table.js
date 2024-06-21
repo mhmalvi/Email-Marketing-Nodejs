629be02aafc5808b14c2a228bd5a9544be56bbb7
TanjibQuad <tanjib@quadque.tech>
2024-06-21 16:30:00 +0600

    migration file

--- migrations/20240621093336-add_user_id_to_tokens_table.js
+++ migrations/20240621093336-add_user_id_to_tokens_table.js
@@ -14,7 +14,7 @@ module.exports = {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {
-        model: User, // name of the source table
+        tableName: 'Users', // name of the source table
         key: "id", // key in the source table that the foreign key references
       },
       onUpdate: "CASCADE",
