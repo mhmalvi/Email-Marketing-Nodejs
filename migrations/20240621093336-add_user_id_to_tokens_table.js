8f37fd94d36b24a182434ffdfaf90486c2fd8f7e
TanjibQuad <tanjib@quadque.tech>
2024-06-21 16:09:09 +0600

    migration file

--- migrations/20240621093336-add_user_id_to_tokens_table.js
+++ migrations/20240621093336-add_user_id_to_tokens_table.js
@@ -15,6 +15,7 @@ module.exports = {
       references: {
         model: {
           tableName: "Users", // name of the source table
+          schema: "public",
         }, // name of the source table
         key: "id", // key in the source table that the foreign key references
       },
