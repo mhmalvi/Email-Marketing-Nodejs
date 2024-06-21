b3b9560967cd8de0d5548af96b9b412528c8989b
TanjibQuad <tanjib@quadque.tech>
2024-06-21 16:34:18 +0600

    migration file

--- models/token.js
+++ models/token.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Token extends Model {
     /**
@@ -10,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Token.belongsTo(models.User, { foreignKey: "userID" });
     }
   }
   Token.init(
