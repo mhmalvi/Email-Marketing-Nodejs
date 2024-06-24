bf22614651b8fd70a042c9ada13d3f00319c511c
TanjibQuad <tanjib@quadque.tech>
2024-06-24 10:56:40 +0600

    login fix

--- migrations/20240428154324-create_token_table.js
+++ migrations/20240428154324-create_token_table.js
@@ -10,14 +10,16 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
     await queryInterface.createTable(
-      "users",
+      "tokens",
       {
-        id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-        userName: { type: Sequelize.TEXT, allowNull: false },
-        googleId: { type: Sequelize.STRING, allowNull: true },
-        email: { type: Sequelize.STRING, allowNull: false, unique: true },
-        password: { type: Sequelize.STRING, allowNull: true },
-        role: { type: Sequelize.INTEGER, allowNull: false },
+        id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+        email: { type: Sequelize.STRING, allowNull: false },
+        ip: { type: Sequelize.STRING, allowNull: false },
+        token: { type: Sequelize.STRING, allowNull: false },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
         createdAt: {
           type: Sequelize.DATE,
           allowNull: false,
@@ -40,6 +42,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.dropTable("users");
+    await queryInterface.dropTable("tokens");
   },
 };
