2c3fbecd5259e935da571e61aa2cf2d5c4a4d1e0
TanjibQuad <tanjib@quadque.tech>
2024-06-27 16:21:46 +0600

    template update api done

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -12,11 +12,12 @@ module.exports.isUserAuthenticated = async (req, res, next) => {
 module.exports.isCustomerAuthenticated = async (req, res, next) => {
   console.log(req);
   const bearerHeader = req.headers["authorization"];
+  var userID = null;
   if (bearerHeader) {
     if (req.body.userID) {
-      const userID = req.body.userID;
+      userID = req.body.userID;
     } else if (req.body.user_id) {
-      const userID = req.body.user_id;
+      userID = req.body.user_id;
     }
     const isTokenExist = await Token.findOne({
       where: { token: bearerHeader, userId: userID },
