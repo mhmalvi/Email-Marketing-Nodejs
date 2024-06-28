8908ba2702026ae3bc63ab3aed18638394574954
TanjibQuad <tanjib@quadque.tech>
2024-06-28 11:45:31 +0600

    signup fix

--- routes/auth-routes-tmp.js
+++ routes/auth-routes-tmp.js
@@ -98,7 +98,7 @@ authRouter.get("/success", isLoggedIn, async (req, res) => {
       photo: req.user.picture,
     });
 
-    console.log('userName.id',userName.id);
+    console.log("userName.id", newUser.id);
 
     // return req.user.email;
     await saveToken(data);
