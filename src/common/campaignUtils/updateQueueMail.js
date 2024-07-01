63dfbc49f4fa194d7578228df18cabcf9e56d9e7
TanjibQuad <tanjib@quadque.tech>
2024-07-01 11:20:01 +0600

    subscription status done

--- src/common/campaignUtils/updateQueueMail.js
+++ src/common/campaignUtils/updateQueueMail.js
@@ -2,7 +2,7 @@ const Emailqueue = require("../../../models").EmailQueue;
 
 const updateDeliveryStatus = async (id) => {
   return await Emailqueue.update(
-    { deliver: 1 },
+    { deliver: 1, subscription_status: 1 }, /////////1= subscribed, 2 = bounced, 0 = unsubscribed
     { where: { id: id, deliver: 0 } }
   );
 };
@@ -10,7 +10,7 @@ const updateDeliveryStatus = async (id) => {
 const updateBounceStatus = async (id) => {
   console.log(id);
   return await Emailqueue.update(
-    { deliver: 2, bounce: 1 },
+    { deliver: 2, bounce: 1, subscription_status: 2 }, ////////// 1= subscribed, 2 = bounced, 0 = unsubscribed
     { where: { id: id, deliver: 0 } }
   );
 };
