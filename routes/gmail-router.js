09ebcf5f6accdea3194db5998873ab5d143bf46b
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-07-02 22:49:19 +0600

    optimization

--- routes/gmail-router.js
+++ routes/gmail-router.js
@@ -17,16 +17,6 @@ const {
 } = require("../src/controllers/AuthController");
 
 //////////////////// routers  start ////////////////
-// gmailRouter.route("/google/login").get(login);
-// gmailRouter.route("/google/callback").get(callback);
-// gmailRouter.route("/google-logout").get(logout);
-// gmailRouter.route("/list").get(list);
-
-gmailRouter.route("/mail/user/:email").get(getUser);
-gmailRouter.route("/mail/send").get(sendMail);
-gmailRouter.route("/mail/drafts/:email").get(getDrafts);
-gmailRouter.route("/mail/read/:messageId").get(readMail);
-gmailRouter.route("/auth/user").get(retrieveUser);
 
 gmailRouter.route("/check-if-user-email-exists").post(isUserEmailExists);
 gmailRouter.route("/verify-otp").post(verifyOTP);
