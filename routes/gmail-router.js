4ef3790a492876c9f19d4012f5b18cccaedc43da
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-07-02 23:17:50 +0600

    optimization

---
+++ routes/gmail-router.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const gmailRouter = express.Router();
+const {
+  validateNotificationStoreData,
+} = require("../src/middleware/notificationStoreValidator");
+const {
+  logout,
+  isUserEmailExists,
+  verifyOTP,
+} = require("../src/controllers/AuthController");
+
+//////////////////// routers  start ////////////////
+
+gmailRouter.route("/check-if-user-email-exists").post(isUserEmailExists);
+gmailRouter.route("/verify-otp").post(verifyOTP);
+gmailRouter.route("/logout").post(logout);
+
+//////////////////// routers  end ///////////////
+
+module.exports = { gmailRouter };
