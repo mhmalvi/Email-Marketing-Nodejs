32563ee7e8ae885084d19a0948303649a2b9ace6
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-07-03 22:30:01 +0600

    bug fix

--- index.js
+++ index.js
@@ -94,8 +94,35 @@ server.listen(port, () => console.log("server running on port" + port));
 // Welcome{"provider":"google","sub":"105703349436150658184","id":"105703349436150658184","displayName":"tanjib Rubyat","name":{"givenName":"tanjib","familyName":"Rubyat"},"given_name":"tanjib","family_name":"Rubyat","email_verified":true,"verified":true,"language":"en-GB","email":"tanjibrubyat@gmail.com","emails":[{"value":"tanjibrubyat@gmail.com","type":"account"}],"photos":[{"value":"https://lh3.googleusercontent.com/a/ACg8ocJQYSJH17nYxP9tIGKVyRRzPDPmTQopLs7RjfY80g2PqQ3SNC8=s96-c","type":"default"}],"picture":"https://lh3.googleusercontent.com/a/ACg8ocJQYSJH17nYxP9tIGKVyRRzPDPmTQopLs7RjfY80g2PqQ3SNC8=s96-c","_raw":"{\n \"sub\": \"105703349436150658184\",\n \"name\": \"tanjib Rubyat\",\n \"given_name\": \"tanjib\",\n \"family_name\": \"Rubyat\",\n \"picture\": \"https://lh3.googleusercontent.com/a/ACg8ocJQYSJH17nYxP9tIGKVyRRzPDPmTQopLs7RjfY80g2PqQ3SNC8\\u003ds96-c\",\n \"email\": \"tanjibrubyat@gmail.com\",\n \"email_verified\": true,\n \"locale\": \"en-GB\"\n}","_json":{"sub":"105703349436150658184","name":"tanjib Rubyat","given_name":"tanjib","family_name":"Rubyat","picture":"https://lh3.googleusercontent.com/a/ACg8ocJQYSJH17nYxP9tIGKVyRRzPDPmTQopLs7RjfY80g2PqQ3SNC8=s96-c","email":"tanjibrubyat@gmail.com","email_verified":true,"locale":"en-GB"}}
 
 ////// socket connection starts ////////
-io.on("connection", async (socket) => {
+io.on("connection", async(socket) => {
   const users = {};
   console.log("a user connected");
-  await campaignSearchRealtime(socket);
+  await campaignSearchRealtime(socket)
+  socket.on("campaigns", (data) => {
+    const { userID, page, per_page, name } = data;
+    const offset = (page - 1) * per_page;
+    // users[userId] = socket.id;
+    console.log("socket id", socket.id);
+    const searchCampaign = async (req, res) => {
+      console.log(userID);
+      const campaigns = await campaignSearch(data);
+      const paginated = await campaignSearchPagination(
+        userID,
+        per_page,
+        offset,
+        name
+      );
+      const socketId = socket.id;
+      const totalPages = campaigns.length / per_page;
+      const count = campaigns.length;
+      const paginate = {
+        paginatedData: paginated,
+        current_page: page,
+        count: count,
+        totalPages: Math.ceil(totalPages),
+      };
+      io.to(socketId).emit("campaigns", paginate);
+    };
+    searchCampaign();
+  });
 });
