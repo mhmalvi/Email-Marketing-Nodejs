67d665a3f1cda019dcfee4de7aab6d1c0a4013b5
TanjibQuad <tanjib@quadque.tech>
2024-07-10 14:36:01 +0600

    campaign total analytics

--- routes/recipients-routes.js
+++ routes/recipients-routes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const {
   allRecipients,
 } = require("../src/controllers/Campaigns/AllRecipientsFetchController");
+const { isCustomerAuthenticated } = require("../src/middleware/userMiddleware");
 const recipientsRouter = express.Router();
 
-recipientsRouter.route("/get-all-recipients-campaignwise").post();
+recipientsRouter
+  .route("/get-all-recipients-campaignwise")
+  .post(isCustomerAuthenticated, allRecipients);
 
 module.exports = { recipientsRouter };
