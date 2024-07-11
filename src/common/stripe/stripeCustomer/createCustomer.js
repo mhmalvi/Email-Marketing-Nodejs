c04117eb0cbd16c84636068479bef9cde57ef864
TanjibQuad <tanjib@quadque.tech>
2024-07-11 15:52:32 +0600

    stripe payment

--- src/common/stripe/stripeCustomer/createCustomer.js
+++ src/common/stripe/stripeCustomer/createCustomer.js
@@ -4,11 +4,11 @@ const stripe_key = Stripe(
   "sk_test_51OtiFcKvZ2nwhLRdtgSm2Kg86tYvxxk0EprDLOKyvqQaZ5ckR3yvjAmQxoff7RuWc2bBHdpv1c56wutQin2b2IYk00jbIXmUId"
 );
 
-const create = async (req, res) => {
-    const customer = await stripe.customers.create({
-      name: "Jenny Rosen",
-      email: "jennyrosen@example.com",
-    });
+const create = async (email, name) => {
+  return await stripe.customers.create({
+    name: name,
+    email: email,
+  });
 };
 
 module.exports = { create };
