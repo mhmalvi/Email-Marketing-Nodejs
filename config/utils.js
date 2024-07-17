6ccd4d3dca459268c49cbbdc6135aa51f6f2ea59
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-07-17 13:01:13 +0600

    test

--- config/utils.js
+++ config/utils.js
@@ -39,7 +39,7 @@ function generateOTP() {
 // });
 const transporter = nodemailer.createTransport({
   host: "170.64.177.8",
-  port: 587,
+  port: 25,
   secure: false, // Use `true` for port 465, `false` for all other ports
   auth: {
     // user: keys.mail.user,
