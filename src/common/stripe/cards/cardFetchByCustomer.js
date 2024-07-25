52e716cd58554c9efb18121f73eabd2ec0654e62
TanjibQuad <tanjib@quadque.tech>
2024-07-25 11:42:14 +0600

    cron

---
+++ src/common/stripe/cards/cardFetchByCustomer.js
@@ -0,0 +1,11 @@
+const Stripe = require("stripe");
+const stripe = require("../../../../config/keys");
+const stripe_key = Stripe(
+  "sk_test_51OtiFcKvZ2nwhLRdtgSm2Kg86tYvxxk0EprDLOKyvqQaZ5ckR3yvjAmQxoff7RuWc2bBHdpv1c56wutQin2b2IYk00jbIXmUId"
+);
+
+const cardFetch = async (req, res) => {
+    
+};
+
+module.exports = { cardFetch };
