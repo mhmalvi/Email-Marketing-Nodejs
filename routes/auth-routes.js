e3297ff49223c3e77d37396589761d5162716d61
TanjibQuad <tanjib@quadque.tech>
2024-07-27 14:45:12 +0600

    subscription

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -118,6 +118,7 @@ authRouter.get("/success", isLoggedIn, async (req, res) => {
       where: { userID: user.id },
     });
     const token = await saveToken(data);
+    console.log(subscription.price);
     res.redirect(
       `https://www.quemailer.com/auth?userName=${req.user.displayName}&email=${req.user.email}&userID=${user.id}&photo=${req.user.picture}&token=${token.token}&first_user=${user.first_user}&subscription=${subscription.subscriptionID}&priceID=${subscription.price}&stripeCustomerID=${user.stripeCustomerID}`
     );
