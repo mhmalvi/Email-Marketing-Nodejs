8af0565dba2880cc8746f4fd7ca4db526b155675
TanjibQuad <tanjib@quadque.tech>
2024-07-28 14:27:14 +0600

    retrieve subscription

--- src/controllers/Stripe/Subscription/retrievePackageController.js
+++ src/controllers/Stripe/Subscription/retrievePackageController.js
@@ -19,7 +19,7 @@ const retrieveCurrentPackageInfo = async (req, res) => {
   } else {
     const subscription = await retrieveSingleSubscription(subscriptionID); /////  fetch a subscription
     //   console.log(subscription.subscriptionID);
-      res.json(subscription.items.data.plan.id);
+      res.json(subscription.items.data[0].plan.id);
     // const price = await retrievePrice(subscription.id);
     // res.json(price);
   }
