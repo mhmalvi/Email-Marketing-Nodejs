05aac277d63dda39bcd770ed0b04615b20679a6f
TanjibQuad <tanjib@quadque.tech>
2024-07-30 11:39:47 +0600

    toggle collection method

--- routes/subscription-routes.js
+++ routes/subscription-routes.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const {
   createSubscription,
+  test,
 } = require("../src/controllers/Stripe/Subscription/createSubscriptionController");
 const {
   retrieveCurrentPackageInfo,
 } = require("../src/controllers/Stripe/Subscription/retrievePackageController");
+const {
+  test,
+} = require("../src/controllers/Stripe/Subscription/testController");
 const { isCustomerAuthenticated } = require("../src/middleware/userMiddleware");
 const subscriptionRoutes = express.Router();
 ////////////////////////////////////////////////////////
-
 subscriptionRoutes
   .route("/create-subscription")
   .post(isCustomerAuthenticated, createSubscription);
 subscriptionRoutes
   .route("/retrieve-current-package-info")
   .post(isCustomerAuthenticated, retrieveCurrentPackageInfo);
+subscriptionRoutes.route("/test").get(test);
 
 module.exports = { subscriptionRoutes };
