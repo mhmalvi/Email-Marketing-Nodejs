f36cd3d1526e79ed89a51dcef8eaa134e50a74ec
TanjibQuad <tanjib@quadque.tech>
2024-07-30 14:22:52 +0600

    toggle collection method

--- src/controllers/Stripe/Subscription/testController.js
+++ src/controllers/Stripe/Subscription/testController.js
@@ -1,9 +1,11 @@
 const test = async (req, res) => {
-    const date = new Date();
-    const formattedDate = date.toISOString().split("T")[0];
-    // const seconds = Math.floor(date.getTime() / 1000);
-    
-    res.json(formattedDate);
+  const date = new Date();
+  const formattedDate = date.toISOString().split("T")[0];
+  // const seconds = Math.floor(date.getTime() / 1000);
+  const subscription = await stripe.subscriptions.retrieve(
+    "sub_1PiAhYKvZ2nwhLRd77kPChW9"
+  );
+  res.json(subscription);
 };
 
 module.exports = { test };
