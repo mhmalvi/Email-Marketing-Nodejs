b3c83df9012c8e8cae0d32114a96b3ec25b8386f
TanjibQuad <tanjib@quadque.tech>
2024-07-30 14:10:47 +0600

    toggle collection method

--- src/controllers/Stripe/Subscription/testController.js
+++ src/controllers/Stripe/Subscription/testController.js
@@ -2,7 +2,8 @@ const test = async (req, res) => {
     const date = new Date();
     const formattedDate = date.toISOString().split("T")[0];
     // const seconds = Math.floor(date.getTime() / 1000);
-    console.log(formattedDate);
+    
+    res.json(formattedDate);
 };
 
 module.exports = { test };
