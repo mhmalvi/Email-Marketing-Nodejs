062c3ab3bc80677d56b474a58af37d6489935666
TanjibQuad <tanjib@quadque.tech>
2024-07-30 14:24:00 +0600

    toggle collection method

--- src/controllers/Stripe/Subscription/testController.js
+++ src/controllers/Stripe/Subscription/testController.js
@@ -1,8 +1,13 @@
+const Stripe = require("stripe");
+const stripe = require("../../../../config/keys");
+const stripe_key = Stripe(
+  "sk_test_51OtiFcKvZ2nwhLRdtgSm2Kg86tYvxxk0EprDLOKyvqQaZ5ckR3yvjAmQxoff7RuWc2bBHdpv1c56wutQin2b2IYk00jbIXmUId"
+);
 const test = async (req, res) => {
   const date = new Date();
   const formattedDate = date.toISOString().split("T")[0];
   // const seconds = Math.floor(date.getTime() / 1000);
-  const subscription = await stripe.subscriptions.retrieve(
+  const subscription = await stripe_key.subscriptions.retrieve(
     "sub_1PiAhYKvZ2nwhLRd77kPChW9"
   );
   res.json(subscription);
