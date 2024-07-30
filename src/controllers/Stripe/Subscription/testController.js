31962be6f80da0097ace36afa07ca1d09ac38489
TanjibQuad <tanjib@quadque.tech>
2024-07-30 15:28:54 +0600

    toggle collection method

--- src/controllers/Stripe/Subscription/testController.js
+++ src/controllers/Stripe/Subscription/testController.js
@@ -7,15 +7,34 @@ const stripe_key = Stripe(
 
 const test = async (req, res) => {
   const date = new Date();
-  const formattedDate = date.toISOString().split("T")[0];
-  // const seconds = Math.floor(date.getTime() / 1000);
+    //   const formattedDate = date.toISOString().split("T")[0];
+    const formattedDate = '2024-08-30'
+  const date_in_seconds = Math.floor(formattedDate.getTime() / 1000);
   const subscription = await stripe_key.subscriptions.retrieve(
     "sub_1PiAhYKvZ2nwhLRd77kPChW9"
   );
-  let milliseconds = subscription.current_period_end * 1000;
-  let endDate = new Date(milliseconds);
-  const formattedEndDate = endDate.toISOString().split("T")[0];
+
   const subscriptions = await Subscribe.findAll({});
+  subscriptions.forEach(async (data) => {
+    let milliseconds = data.current_period_end * 1000;
+    //   let endDate = new Date(milliseconds);
+      const milliseconds_in_seconds = Math.floor(milliseconds / 1000);
+      const formattedEndDate = endDate.toISOString().split("T")[0];
+      
+    if (date_in_seconds == milliseconds_in_seconds) {
+    //   Subscribe.update(
+    //       {
+    //         product_selection_status:
+    //     },
+    //     {
+    //       userID: data.userID,
+    //     }
+        //   );
+        return true
+    } else {
+        return false
+    }
+  });
   res.json(subscriptions);
 };
 
