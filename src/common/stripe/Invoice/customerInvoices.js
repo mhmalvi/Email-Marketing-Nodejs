1c397755e7abfe5dc563f000cf4b649840d83a5e
TanjibQuad <tanjib@quadque.tech>
2024-07-31 09:33:26 +0600

    stripe price fetch fix

--- src/common/stripe/Invoice/customerInvoices.js
+++ src/common/stripe/Invoice/customerInvoices.js
@@ -1,9 +1,8 @@
 const Stripe = require("stripe");
 const stripe = require("../../../../config/keys");
-const stripe_key = process.env.STRIPE_KEY;
+const stripe_key = Stripe(process.env.STRIPE_KEY);
 
 const customerInvoice = async (customerID, status, limit) => {
-  console.log("common", stripe_key);
   return await stripe_key.invoices.list({
     customer: customerID,
     status: status,
