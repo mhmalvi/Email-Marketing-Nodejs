fd4f16008f6e811d81e885619093e605a42ba2f7
TanjibQuad <tanjib@quadque.tech>
2024-07-31 08:54:24 +0600

    stripe price fetch fix

--- routes/stripePrice-routes.js
+++ routes/stripePrice-routes.js
@@ -4,9 +4,6 @@ const {
   priceByProductID,
   retrievePrices,
 } = require("../src/controllers/Stripe/Price/retrievePriceController");
-const {
-  retrievePrices,
-} = require("../src/controllers/Stripe/Products/retrieveProductsController");
 
 stripePriceRouter.route("/stripe-price-by-product-id").post(priceByProductID);
 stripePriceRouter.route("/stripe-prices").post(retrievePrices);
