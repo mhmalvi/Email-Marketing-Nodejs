08fe02420cde6fc82fdeb99e9d1365ebfcae0675
TanjibQuad <tanjib@quadque.tech>
2024-07-31 17:45:28 +0600

    stripe cancel subscription

--- src/controllers/Stripe/Subscription/cancelSubscriptionController.js
+++ src/controllers/Stripe/Subscription/cancelSubscriptionController.js
@@ -1,7 +1,8 @@
 const { fieldsValidation } = require("../../../../config/utils");
 
 const cancelSubscription = async (req, res) => {
-    console.log('qq');
+    // console.log('qq');
+    
 };
 
 module.exports = { cancelSubscription };
