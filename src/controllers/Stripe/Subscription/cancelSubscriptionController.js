f30c001b29b0e3bcf354e977a8850c0895b1ff9c
TanjibQuad <tanjib@quadque.tech>
2024-08-01 15:55:39 +0600

    cancel subscription

--- src/controllers/Stripe/Subscription/cancelSubscriptionController.js
+++ src/controllers/Stripe/Subscription/cancelSubscriptionController.js
@@ -1,5 +1,4 @@
 const Stripe = require("stripe");
-const stripe = require("../../../../config/keys");
 const { fieldsValidation } = require("../../../../config/utils");
 const stripe_key = Stripe(process.env.STRIPE_KEY);
 const {
