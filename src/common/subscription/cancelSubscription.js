c5a34b09b0cd2f1d1a23f86d108842a22ad61169
TanjibQuad <tanjib@quadque.tech>
2024-08-01 15:56:59 +0600

    cancel subscription

--- src/common/subscription/cancelSubscription.js
+++ src/common/subscription/cancelSubscription.js
@@ -1,6 +1,6 @@
 const Stripe = require("stripe");
 const stripe_key = Stripe(process.env.STRIPE_KEY);
-const Subscribe = require("../../../../models").Subscribe;
+const Subscribe = require("../../../models").Subscribe;
 
 const cancelSubscriptionFromDB = async (userID) => {
   return await Subscribe.update(
