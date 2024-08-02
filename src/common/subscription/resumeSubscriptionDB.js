fc1aa05335c3d936322a38c2d5db7c3ebfa9215e
TanjibQuad <tanjib@quadque.tech>
2024-08-02 12:31:08 +0600

    resume subscription

--- src/common/subscription/resumeSubscriptionDB.js
+++ src/common/subscription/resumeSubscriptionDB.js
@@ -2,7 +2,7 @@ const Stripe = require("stripe");
 const stripe_key = Stripe(process.env.STRIPE_KEY);
 const Subscribe = require("../../../models").Subscribe;
 
-export const resumeSubscriptionDB = async (priceID, expDate, userID) => {
+const resumeSubscriptionDB = async (priceID, expDate, userID) => {
   return await Subscribe.update(
     {
       price: priceID,
