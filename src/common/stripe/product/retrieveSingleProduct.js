7f546a9bbe4b154b4684c992e6e91d2ef9cff02e
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-06 11:35:51 +0600

    convert subscription to free mode

--- src/common/stripe/product/retrieveSingleProduct.js
+++ src/common/stripe/product/retrieveSingleProduct.js
@@ -1,11 +1,9 @@
 const Stripe = require("stripe");
 const stripe = require("../../../../config/keys");
-const stripe_key = Stripe(
-  "sk_test_51OtiFcKvZ2nwhLRdtgSm2Kg86tYvxxk0EprDLOKyvqQaZ5ckR3yvjAmQxoff7RuWc2bBHdpv1c56wutQin2b2IYk00jbIXmUId"
-);
+const stripe_key = Stripe(process.env.STRIPE_KEY);
 
 const retrieveSingleProduct = async (productID) => {
-    return await stripe_key.products.retrieve(productID);
-}
+  return await stripe_key.products.retrieve(productID);
+};
 
 module.exports = { retrieveSingleProduct };
