daf0fdfbeb47e5ce91095bafa81cf312361e099e
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-07 18:04:05 +0600

    counts

--- src/controllers/Counts/LimitController.js
+++ src/controllers/Counts/LimitController.js
@@ -24,9 +24,9 @@ const counts = async (req, res) => {
     res.status(200).json({
       message: "success",
       status: 200,
-      mailCount: mailCount,
+      mailCount: mailCount === "undefined" ? 0 : mailCount,
       contactsCount: contactsCount === "undefined" ? 0 : contactsCount,
-      campaignCount: campaignCount,
+      campaignCount: campaignCount === "undefined" ? 0 : campaignCount,
     });
   }
 };
