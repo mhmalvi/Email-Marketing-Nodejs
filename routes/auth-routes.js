23ae8bcf808806d723ef71dd3153294995950293
TanjibQuad <tanjib@quadque.tech>
2024-08-09 17:02:59 +0600

    subadminh

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = express();
 const passport = require("passport");
 const { engine } = require("express-handlebars");
 const authRouter = express.Router();
