12f599c9b5a7524c309c8374366007b66113ee95
TanjibQuad <tanjib@quadque.tech>
2024-08-12 10:53:13 +0600

    remaining limits count

--- src/controllers/Counts/LimitController.js
+++ src/controllers/Counts/LimitController.js
@@ -4,6 +4,13 @@ const {
   campaignCounts,
 } = require("../../common/campaignUtils/fetchCampaigns");
 const { contactCounts } = require("../../common/contactsUtils/fetch");
+const {
+  retrieveSingleSubscription,
+} = require("../../common/stripe/subscription/retrieveSingleSubscription");
+const {
+  retrieveSubscriptionFromDB,
+} = require("../../common/subscription/retrieveSubscriptionDB");
+const Product = require("../../../models").Product;
 
 const counts = async (req, res) => {
   const { userID } = req.body;
@@ -15,18 +22,34 @@ const counts = async (req, res) => {
       status: 422,
     });
   } else {
+    const subscriptionDB = await retrieveSubscriptionFromDB(userID); //// fetch user subscription from DB
+    var subscriptionName = "";
+    if (subscriptionDB.subscriptionID !== null) {
+      const stripeSubscription = await retrieveSingleSubscription(
+        subscriptionDB.subscriptionID
+      ); ///fetch user subscription from stripe
+      subscriptionName = stripeSubscription.items.data[0].price.lookup_key;
+    } else {
+      subscriptionName = "free";
+    }
+    const productDB = await Product.findOne({
+      where: { productName: subscriptionName },
+    }); /// get all products from db
     const mailCount = await mailCounts(userID); ////get mail count for today
     const contactsCount = await contactCounts(userID); ///get contacts count for today
     const campaignCount = await campaignCounts(userID); //get campaign counts for today
-    console.log("ccampaignCount", campaignCount);
-    console.log("mailCount", mailCount);
-    console.log("contactsCount", contactsCount);
     res.status(200).json({
       message: "success",
       status: 200,
       mailCount: mailCount === "undefined" ? 0 : mailCount,
       contactsCount: contactsCount === "undefined" ? 0 : contactsCount,
       campaignCount: campaignCount === "undefined" ? 0 : campaignCount,
+      remainingMail:
+        mailCount === "undefined" ? 0 : productDB.emailLimit - mailCount,
+      remainingContact:
+        contactsCount === "undefined"
+          ? 0
+          : productDB.contactLimit - contactsCount,
     });
   }
 };
