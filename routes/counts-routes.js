480effbce44ff3a3f4e4027406d20b52569e473b
TanjibQuad <tanjib@quadque.tech>
2024-08-12 11:36:10 +0600

    remaining limits count

--- routes/counts-routes.js
+++ routes/counts-routes.js
@@ -3,6 +3,6 @@ const { counts } = require("../src/controllers/Counts/LimitController");
 const countsRouter = express.Router();
 const { isCustomerAuthenticated } = require("../src/middleware/userMiddleware");
 
-countsRouter.route("/counts").post(counts);
+countsRouter.route("/counts").post(isCustomerAuthenticated, counts);
 
 module.exports = { countsRouter };
