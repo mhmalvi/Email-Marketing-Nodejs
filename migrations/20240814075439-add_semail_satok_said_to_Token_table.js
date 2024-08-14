4501b571f7e2f7bee565f633a4fa99139ce32f92
TanjibQuad <tanjib@quadque.tech>
2024-08-14 14:04:21 +0600

    migration

--- migrations/20240814075439-add_semail_satok_said_to_Token_table.js
+++ migrations/20240814075439-add_semail_satok_said_to_Token_table.js
@@ -1,22 +1,34 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.addColumn("tokens", "semail", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("tokens", "satok", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("tokens", "said", {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+  },
 };
