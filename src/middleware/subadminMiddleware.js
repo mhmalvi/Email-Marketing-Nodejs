f0cb0b23ca52d7bcd7af7535a9d66bdcce55429e
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-15 13:02:31 +0600

    subadmin login to company

---
+++ src/middleware/subadminMiddleware.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const Token = require("../../models").Token;
+
+module.exports.isUserAuthenticated = async (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.status(401).send("You must login");
+  }
+};
+
+module.exports.isSubadminAuthenticated = async (req, res, next) => {
+  console.log(req);
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader) {
+    const isTokenExist = await Token.findOne({
+      where: { satok: bearerHeader, said: said },
+    });
+    if (isTokenExist) {
+      next();
+    } else {
+      res.status(401).send("You must login");
+    }
+  } else {
+    res.status(401).send("You must login");
+  }
+};
