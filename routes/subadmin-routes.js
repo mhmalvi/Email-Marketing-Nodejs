bba23109dba1f1d3021b3c514a446f6c32311970
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-15 13:03:22 +0600

    subadmin login to company

--- routes/subadmin-routes.js
+++ routes/subadmin-routes.js
@@ -5,10 +5,15 @@ const {
 const {
   subAdminLoginToCompany,
 } = require("../src/controllers/SubAdmin/SubAdminLoginToCompanyController");
+const {
+  isSubadminAuthenticated,
+} = require("../src/middleware/subadminMiddleware");
 const subadminRouter = express.Router();
 
 subadminRouter.route("/create-subadmin").post(createSubAdmin);
 
-subadminRouter.route("/subadminToCompany").post(subAdminLoginToCompany);
+subadminRouter
+  .route("/subadminToCompany")
+  .post(isSubadminAuthenticated, subAdminLoginToCompany);
 
 module.exports = subadminRouter;
