9feab8a5193d6babb5bd23fe08111d08d4c17269
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-15 14:40:53 +0600

    fetch company subadmins

--- src/controllers/SubAdmin/fetchSubadminsController.js
+++ src/controllers/SubAdmin/fetchSubadminsController.js
@@ -34,7 +34,7 @@ const fetchSubAdminByCompany = async (userID) => {
   return await Subadmin.findAll({
     where: {
       userID: {
-        [Op.contains]: JSON.stringify([userID]),
+        [Op.contains]: ([userID]),
       },
     },
   });
