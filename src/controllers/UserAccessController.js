a19261af1ca4b3a0818b3bb7fc2396269915c2c8
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-16 12:13:18 +0600

    remove subadmin

--- src/controllers/UserAccessController.js
+++ src/controllers/UserAccessController.js
@@ -4,7 +4,6 @@ const userAccess = async (req, res) => {
   const bearerHeader = req.headers["authorization"];
   console.log(bearerHeader);
   const token = await Token.findOne({where:{token:bearerHeader}})
-  res.json(token.satok)
   if(token.satok===null){
     res.status(200).json({
         message:'customer'
