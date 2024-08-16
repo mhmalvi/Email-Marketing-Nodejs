2eae7164e9f96b7c8b9be42ecad45f70aa7321f7
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-16 12:08:05 +0600

    remove subadmin

--- routes/getID-routes.js
+++ routes/getID-routes.js
@@ -5,6 +5,6 @@ const { getID } = require("../src/controllers/Stripe/GetID/stripeIDController");
 const { userAccess } = require("../src/controllers/UserAccessController");
 
 getIDRouter.route("/getID").post(isCustomerAuthenticated, getID);
-getIDRouter.route("/getUser").post(isCustomerAuthenticated, userAccess);
+getIDRouter.route("/getUser").post( userAccess);
 
 module.exports = { getIDRouter };
