0ef3ba2dd4b643dbe607ba169ce05a405b7a9c07
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-08-16 12:11:15 +0600

    remove subadmin

--- src/controllers/UserAccessController.js
+++ src/controllers/UserAccessController.js
@@ -3,5 +3,10 @@ const Token = require("../../models").Token;
 const userAccess = async (req, res) => {
   const bearerHeader = req.headers["authorization"];
   console.log(bearerHeader);
+  const token = await Token.findOne({where:{token:bearerHeader}})
+  res.json(token.satok)
+  if(token){
+
+  }
 };
 module.exports = { userAccess };
