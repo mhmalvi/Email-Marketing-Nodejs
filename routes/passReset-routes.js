193a6d6668ba7f7523c11ba7c3ec2d6b03ec3fbb
TanjibQuad <tanjib@quadque.tech>
2024-08-22 14:18:35 +0600

    forget password

--- routes/passReset-routes.js
+++ routes/passReset-routes.js
@@ -3,7 +3,7 @@ const passReset = express.Router();
 const Subadmin = require("../models").Subadmin;
 const User = require("../models").User;
 
-passReset.post("/reset-password/:token", async (req, res) => {
+passReset.get("/reset-password/:token", async (req, res) => {
   const { token } = req.params;
   const user = await User.findOne({ where: { pass_reset_token: token } });
   const subadmin = await Subadmin.findOne({
