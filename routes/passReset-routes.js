c8114cc5928c9b3b29205defb2158e585892dfcf
TanjibQuad <tanjib@quadque.tech>
2024-08-22 16:15:42 +0600

    password reset

--- routes/passReset-routes.js
+++ routes/passReset-routes.js
@@ -34,7 +34,7 @@ passReset.get("/reset-password/:token", async (req, res) => {
 });
 
 passReset.get("/new-password", async (req, res) => {
-    console.log("hello world", req);
+    console.log("hello world", req.body);
     
   const { email, password } = req.body;
   if (email && password) {
