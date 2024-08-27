f2fc7dba28a6139bbfbb8472abe92220d714964f
TanjibQuad <tanjib@quadque.tech>
2024-08-27 11:00:03 +0600

    contact us save

--- routes/contactus-routes.js
+++ routes/contactus-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { insertContactUs } = require("../src/controllers/ContactUs/ContactUsController");
 const contactusRoutes = express.Router();
 
 contactusRoutes.route("/save-contactus").post(insertContactUs);
