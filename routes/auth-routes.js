6810be3983b00abec9462d553ab1ea1cf2ccf590
TanjibQuad <tanjib@quadque.tech>
2024-08-27 14:19:48 +0600

    superadmin auth

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -115,19 +115,22 @@ authRouter.get("/success", isLoggedIn, async (req, res) => {
       );
     }
   } else {
-    data.userID = user.id;
-    console.log("data", data.role);
-    
+    data = {
+      email: user.email,
+      token: token,
+      googleId: user.id,
+      userName: user.displayName,
+      role: user.role,
+      photo: user.picture,
+      userID : user.id
+    };
+
     if (user.first_user == 1) {
       user.first_user = 0;
       user.save();
     }
-    // const subscription = await Subscribe.findOne({
-    //   where: { userID: user.id },
-    // });
     const token = await saveToken(data);
-    console.log(token);
-    
+
     res.redirect(
       `https://www.quemailer.com/auth?userName=${req.user.displayName}&email=${req.user.email}&userID=${user.id}&photo=${req.user.picture}&token=${token.token}&first_user=${user.first_user}`
     );
