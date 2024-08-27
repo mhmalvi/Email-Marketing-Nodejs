9b7c5b86aa6a3993e1818d9301003946fd9a821d
TanjibQuad <tanjib@quadque.tech>
2024-08-27 12:40:54 +0600

    superadmin auth

--- src/common/utils.js
+++ src/common/utils.js
@@ -3,7 +3,7 @@ const Token = require("../../models").Token;
 async function saveToken(data) {
   return await Token.create({
     email: data.email,
-    role: role,
+    role: data.role,
     token: data.token,
     userId: data.userID,
   });
@@ -11,7 +11,7 @@ async function saveToken(data) {
 async function saveSubAdminToken(data) {
   return await Token.create({
     semail: data.email,
-    role: role,
+    role: data.role,
     satok: data.token,
     said: data.userID,
   });
