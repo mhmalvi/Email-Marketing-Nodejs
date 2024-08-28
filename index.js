1073ce260f75e38cd2ee6809f070e2cc05e7d595
TanjibQuad <tanjib@quadque.tech>
2024-08-28 12:48:01 +0600

    bulk insert

--- index.js
+++ index.js
@@ -90,7 +90,8 @@ app
 // app.get("/", (req, res) => {
 //   res.render("mail", { layout: "mail" });
 // });
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "100mb" })); // Adjust the limit as needed
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app
   .use("/google", authRouter)
   .use(
