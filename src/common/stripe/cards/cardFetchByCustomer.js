1dc2feada17006260fe9f1a2a1e3be43f8283aab
TanjibQuad <tanjib@quadque.tech>
2024-08-30 12:27:23 +0600

    subscription send invoice

--- src/common/stripe/cards/cardFetchByCustomer.js
+++ src/common/stripe/cards/cardFetchByCustomer.js
@@ -1,8 +1,6 @@
 const Stripe = require("stripe");
-const stripe = require("../../../../config/keys");
-const stripe_key = Stripe(
-  "sk_test_51OtiFcKvZ2nwhLRdtgSm2Kg86tYvxxk0EprDLOKyvqQaZ5ckR3yvjAmQxoff7RuWc2bBHdpv1c56wutQin2b2IYk00jbIXmUId"
-);
+// const stripe = require("../../../../config/keys");
+const stripe_key = Stripe(process.env.STRIPE_KEY);
 
 const cardFetch = async (req, res) => {
     
--- src/controllers/Stripe/GetID/stripeIDController.js
+++ src/controllers/Stripe/GetID/stripeIDController.js
@@ -2,9 +2,7 @@ const Stripe = require("stripe");
 const stripe = require("../../../../config/keys");
 const Subscribe = require("../../../../models").Subscribe;
 const User = require("../../../../models").User;
-const stripe_key = Stripe(
-  "sk_test_51OtiFcKvZ2nwhLRdtgSm2Kg86tYvxxk0EprDLOKyvqQaZ5ckR3yvjAmQxoff7RuWc2bBHdpv1c56wutQin2b2IYk00jbIXmUId"
-);
+const stripe_key = Stripe(process.env.STRIPE_KEY);
 const { fieldsValidation } = require("../../../../config/utils");
 const {
   retrieveSingleProduct,
