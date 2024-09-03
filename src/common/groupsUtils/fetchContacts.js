4d5d7cc0e4d3fbbf0bc9fef8f0afb378a1a9d6af
TanjibQuad <tanjib@quadque.tech>
2024-09-03 12:38:50 +0600

    group fetch and destroy

--- src/common/groupsUtils/fetchContacts.js
+++ src/common/groupsUtils/fetchContacts.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 const Contact = require("../../../models").Contact;
 
 module.exports.fetchContacts = async (user_id) => {
@@ -6,12 +6,10 @@ module.exports.fetchContacts = async (user_id) => {
 };
 
 module.exports.fetchGroupsByID = async (user_id) => {
-  return await Contact.findAll(
-    { where: { user_id: JSON.parse(user_id) } },
-    {
-      attributes: [[Sequelize.fn("DISTINCT", Sequelize.col("group")), "group"]],
-    }
-  );
+  return await Contact.findAll({
+    where: { user_id: JSON.parse(user_id) },
+    attributes: [[Sequelize.fn("DISTINCT", Sequelize.col("group")), "group"]],
+  });
 };
 module.exports.fetchGroupsPagination = async (user_id) => {
   return await Contact.findAll({ where: { user_id: JSON.parse(user_id) } });
