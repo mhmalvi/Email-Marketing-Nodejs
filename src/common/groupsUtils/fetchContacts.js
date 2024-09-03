ad711aa59a6910c0ee77a075f9d63207b5abefdd
TanjibQuad <tanjib@quadque.tech>
2024-09-03 16:06:24 +0600

    group fetch and destroy

--- src/common/groupsUtils/fetchContacts.js
+++ src/common/groupsUtils/fetchContacts.js
@@ -9,11 +9,7 @@ module.exports.fetchGroupsByID = async (user_id) => {
   return await Contact.findAll({
     where: { user_id: JSON.parse(user_id) },
     attributes: [
-      [
-        Sequelize.fn("DISTINCT", Sequelize.col("group", "updatedAt")),
-        "group",
-        "updatedAt",
-      ],
+      [Sequelize.fn("DISTINCT", Sequelize.col("group")), "group", "updatedAt"],
     ],
   });
 };
@@ -23,11 +19,7 @@ module.exports.fetchGroupsByPagination = async (user_id, per_page, offset) => {
     limit: per_page,
     offset: offset,
     attributes: [
-      [
-        Sequelize.fn("DISTINCT", Sequelize.col("group", "updatedAt")),
-        "group",
-        "updatedAt",
-      ],
+      [Sequelize.fn("DISTINCT", Sequelize.col("group")), "group", "updatedAt"],
     ],
   });
 };
