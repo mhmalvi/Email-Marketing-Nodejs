c8ab4a8642914316af4d4a67452eca172134acda
TanjibQuad <tanjib@quadque.tech>
2024-09-05 10:11:27 +0600

    group search

--- src/common/groupsUtils/searchGroups.js
+++ src/common/groupsUtils/searchGroups.js
@@ -1,3 +1,7 @@
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+const Contact = require("../../../models").Contact;
+
 module.exports.searchGroups = async (data) => {
   return await Contact.findAll({
     attributes: [[Sequelize.fn("DISTINCT", Sequelize.col("group")), "group"]],
