ef40618a2105689c754dfeb70a75107ef39c9727
TanjibQuad <tanjib@quadque.tech>
2024-09-09 11:32:55 +0600

    fetch group with batchID

--- src/common/groupsUtils/fetchContacts.js
+++ src/common/groupsUtils/fetchContacts.js
@@ -10,7 +10,11 @@ module.exports.fetchGroupsByID = async (user_id) => {
     where: { user_id: JSON.parse(user_id) },
     attributes: [
       "group",
-      [Sequelize.fn("MAX", Sequelize.col("updatedAt")), "updatedAt", "batchID"],
+      [
+        Sequelize.fn("MAX", Sequelize.col("updatedAt", "batchID")),
+        "updatedAt",
+        "batchID",
+      ],
     ],
     group: ["group"],
   });
@@ -22,7 +26,11 @@ module.exports.fetchGroupsByPagination = async (user_id, per_page, offset) => {
     offset: offset,
     attributes: [
       "group",
-      [Sequelize.fn("MAX", Sequelize.col("updatedAt")), "updatedAt", "batchID"],
+      [
+        Sequelize.fn("MAX", Sequelize.col("updatedAt", "batchID")),
+        "updatedAt",
+        "batchID",
+      ],
     ],
     group: ["group"],
   });
