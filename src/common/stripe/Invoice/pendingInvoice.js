f27537c14bc2a58e8e2b89050286952269a84bf5
TanjibQuad <tanjib@quadque.tech>
2024-09-11 10:31:48 +0600

    due checking

--- src/common/stripe/Invoice/pendingInvoice.js
+++ src/common/stripe/Invoice/pendingInvoice.js
@@ -5,6 +5,6 @@ const stripe_key = Stripe(process.env.STRIPE_KEY);
 module.exports.pendingInvoice = async (customerID) => {
   return await stripe_key.invoices.list({
     customer: customerID,
-    status: open,
+    status: "open",
   });
 };
