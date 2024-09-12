c31a81fc28f8d7db985b7502336e986b5c9a9783
TanjibQuad <tanjib@quadque.tech>
2024-09-12 17:56:25 +0600

    mail change

--- src/controllers/Contacts/ContactInsertController.js
+++ src/controllers/Contacts/ContactInsertController.js
@@ -28,7 +28,7 @@ const insertContact = async (req, res) => {
       });
     }
 
-    const chunkSize = 1000; // Define chunk size based on your needs
+    const chunkSize = 100; // Define chunk size based on your needs
     let chunkedContacts = chunkArray(data, chunkSize);
     const batchID = Date.now() + Math.floor(Math.random() * 1000000);
     var batch = "";
