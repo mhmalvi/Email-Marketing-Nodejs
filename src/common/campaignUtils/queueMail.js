47f04320ebff96c532a5bdaa34881dea16a497cd
TanjibQuad <tanjib@quadque.tech>
2024-09-12 16:47:36 +0600

    mail change

--- src/common/campaignUtils/queueMail.js
+++ src/common/campaignUtils/queueMail.js
@@ -44,8 +44,6 @@ const queueMail = async (data, campaignID) => {
 const fetchQueuedMails = async () => {
   return await Emailqueue.findAll({
     where: { deliver: 0 },
-    order: [["id", "DESC"]],
-    limit:10
   });
 };
 
