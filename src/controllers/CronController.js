f3c4017be518a993abf3f77e0ed2554b86c87e2b
Tanjib Rubyat <tanjibrubyat@quadque.tech>
2024-09-13 23:31:57 +0600

    latest bug fix

--- src/controllers/CronController.js
+++ src/controllers/CronController.js
@@ -8,7 +8,7 @@ const {
 } = require("../common/cron/CronMethods/blockSubscriptionCron");
 const { deleteOTPCron } = require("../common/cron/CronMethods/deleteOTPCron");
 
-cron.schedule("*/5 * * * *", () => {
+cron.schedule("*/20 * * * *", () => {
   sendMail();
   deleteOTPCron();
 });
